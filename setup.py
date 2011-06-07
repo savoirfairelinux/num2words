8f122281c2ecece9454cbe66b043af6ee0d98233
Marius Grigaitis <devnull@localhost>
2011-06-07 17:28:19 +0300

    Added setup.py and restructured module

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pynum2word',
+      version=0.4,
+      description='Modules to convert numbers to words. Easily extensible.',
+      author='Taro Ogawa <tso at users sourceforge net>',
+      author_email='tos@users.sourceforge.net',
+      url='http://pypi.python.org/pypi/PyNum2Word/',
+      packages=['distutils', 'distutils.command'],
+     )
