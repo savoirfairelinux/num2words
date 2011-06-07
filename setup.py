7ad974bfec3f6671c3ecb3ac75bc3ab4440b7462
Marius Grigaitis <devnull@localhost>
2011-06-07 18:05:53 +0300

    removed dependencies

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='pynum2word',
       author='Taro Ogawa <tso at users sourceforge net>',
       author_email='tos@users.sourceforge.net',
       url='http://pypi.python.org/pypi/PyNum2Word/',
-      packages=['distutils', 'distutils.command'],
+      packages=[],
      )
