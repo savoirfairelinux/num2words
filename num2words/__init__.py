0da769c6616cf81598b38a70d49fd08697033d00
isnani <isnanipramusinto@gmail.com>
2015-12-18 03:17:17 +0100

    add indonesian ('id')

--- num2words/__init__.py
+++ num2words/__init__.py
@@ -25,6 +25,7 @@ from . import lang_ES
 from . import lang_LT
 from . import lang_LV
 from . import lang_PL
+from . import lang_ID
 
 CONVERTER_CLASSES = {
     'en': lang_EN.Num2Word_EN(),
@@ -33,9 +34,10 @@ CONVERTER_CLASSES = {
     'fr': lang_FR.Num2Word_FR(),
     'de': lang_DE.Num2Word_DE(),
     'es': lang_ES.Num2Word_ES(),
+    'id': lang_ID.Num2Word_ID(),
     'lt': lang_LT.Num2Word_LT(),
     'lv': lang_LV.Num2Word_LV(),
-    'pl': lang_PL.Num2Word_PL(),
+    'pl': lang_PL.Num2Word_PL()
 }
 
 def num2words(number, ordinal=False, lang='en'):
