b51a708789e8c37849562cbca3eb938d346d49ab
pipeCh <pipech@gmail.com>
2017-11-13 09:39:23 +0700

    add Thai

---
+++ num2words/lang_TH.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2003, Taro Ogawa.  All Rights Reserved.
+# Copyright (c) 2013, Savoir-faire Linux inc.  All Rights Reserved.
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301 USA
+
+from __future__ import unicode_literals
+import decimal
+
+thai_num = (
+    'ศูนย์', 'หนึ่ง', 'สอง', 'สาม', 'สี่',
+    'ห้า', 'หก', 'เจ็ด', 'แปด', 'เก้า'
+)
+
+thai_multiplier = ('', 'สิบ', 'ร้อย', 'พัน', 'หมื่น', 'แสน', 'ล้าน')
+
+
+def six_num_to_text(six_num):
+    length = len(six_num) > 1
+    word_num = ''
+
+    for index, num in enumerate(map(int, six_num)):
+        if num:
+            if index:
+                word_num = thai_multiplier[index] + word_num
+
+            if length and num == 1 and index == 0:
+                word_num += 'เอ็ด'
+            elif index == 1 and num == 2:
+                word_num = 'ยี่' + word_num
+            elif index != 1 or num != 1:
+                word_num = thai_num[num] + word_num
+
+        elif num == 0 and index == 0 and length == 0:
+            word_num = 'ศูนย์'
+
+    return word_num
+
+
+def split_six(num_txt):
+    num_txt = num_txt[::-1]
+    list_six_num = range(0, len(num_txt), 6)
+
+    result = list(map(
+        lambda i: num_txt[i:i+6],  list_six_num
+    ))
+
+    return result
+
+
+def add_text_million(word_num):
+    result = ''
+
+    for index, t in enumerate(reversed(word_num)):
+        if index == 0:
+            result = t
+        else:
+            result = result + 'ล้าน' + t
+
+    return result
+
+
+def round_2_decimal(number):
+    number = decimal.Decimal(number)
+    number = number.quantize(
+        decimal.Decimal('.01'),
+        rounding=decimal.ROUND_HALF_UP
+    )
+
+    text_num = '{:0.2f}'.format(number)
+    return text_num
+
+
+def left_num_to_text(number):
+
+    left_num_list = split_six(number)
+
+    left_text_list = []
+    for i in left_num_list:
+        left_text_list.append(six_num_to_text(i))
+
+    left_text = add_text_million(left_text_list)
+    return left_text
+
+
+def num_to_words(number):
+
+    text_num = '{}'.format(number)
+
+    split_num = text_num.split('.')
+
+    left_num = split_num[0]
+
+    result = left_num_to_text(left_num)
+
+    if len(split_num) > 1:
+        right_num = split_num[1]
+        right_text = ''
+        if not right_num == '0':
+            for i in map(int, right_num):
+                right_text = right_text + thai_num[i]
+            result = result + 'จุด' + right_text
+
+    return result
+
+
+def num_to_currency(number):
+
+    number = round_2_decimal(number)
+
+    split_num = number.split('.')
+
+    left_num = split_num[0]
+    left_text = left_num_to_text(left_num)
+
+    right_num = split_num[1]
+    right_text = six_num_to_text(right_num[::-1].rstrip('0'))
+
+    if right_num == '00':
+        result = left_text + 'บาทถ้วน'
+    else:
+        if left_num == '0':
+            result = right_text + 'สตางค์'
+        else:
+            result = left_text + 'บาท' + right_text + 'สตางค์'
+
+    return result
+
+
+class Num2Word_TH(object):
+    def to_cardinal(self, number):
+        return num_to_words(number)
+
+    def to_ordinal(self, number):
+        return num_to_words(number)
+
+    def to_currency(self, number):
+        return num_to_currency(number)
---
+++ tests/test_th.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from unittest import TestCase
+
+from num2words import num2words
+from num2words.lang_TH import round_2_decimal
+
+
+class TestNumWord(TestCase):
+
+    def test_0(self):
+        self.assertEqual(num2words(0, lang='th'), "ศูนย์")
+
+    def test_end_with_1(self):
+        self.assertEqual(num2words(21, lang='th'), "ยี่สิบเอ็ด")
+        self.assertEqual(num2words(11, lang='th'), "สิบเอ็ด")
+        self.assertEqual(num2words(101, lang='th'), "หนึ่งร้อยเอ็ด")
+        self.assertEqual(num2words(1201, lang='th'), "หนึ่งพันสองร้อยเอ็ด")
+
+    def test_start_20(self):
+        self.assertEqual(num2words(22, lang='th'), "ยี่สิบสอง")
+        self.assertEqual(num2words(27, lang='th'), "ยี่สิบเจ็ด")
+
+    def test_start_10(self):
+        self.assertEqual(num2words(10, lang='th'), "สิบ")
+        self.assertEqual(num2words(18, lang='th'), "สิบแปด")
+
+    def test_1_to_9(self):
+        self.assertEqual(num2words(1, lang='th'), "หนึ่ง")
+        self.assertEqual(num2words(5, lang='th'), "ห้า")
+        self.assertEqual(num2words(9, lang='th'), "เก้า")
+
+    def test_31_to_99(self):
+        self.assertEqual(num2words(31, lang='th'), "สามสิบเอ็ด")
+        self.assertEqual(num2words(48, lang='th'), "สี่สิบแปด")
+        self.assertEqual(num2words(76, lang='th'), "เจ็ดสิบหก")
+
+    def test_100_to_999(self):
+        self.assertEqual(num2words(100, lang='th'), "หนึ่งร้อย")
+        self.assertEqual(num2words(123, lang='th'), "หนึ่งร้อยยี่สิบสาม")
+        self.assertEqual(num2words(456, lang='th'), "สี่ร้อยห้าสิบหก")
+        self.assertEqual(num2words(721, lang='th'), "เจ็ดร้อยยี่สิบเอ็ด")
+
+    def test_1000_to_9999(self):
+        self.assertEqual(num2words(1000, lang='th'), "หนึ่งพัน")
+        self.assertEqual(num2words(2175, lang='th'), "สองพันหนึ่งร้อยเจ็ดสิบห้า")
+        self.assertEqual(num2words(4582, lang='th'), "สี่พันห้าร้อยแปดสิบสอง")
+        self.assertEqual(num2words(9346, lang='th'), "เก้าพันสามร้อยสี่สิบหก")
+
+    def test_10000_to_99999(self):
+        self.assertEqual(
+            num2words(11111, lang='th'), "หนึ่งหมื่นหนึ่งพันหนึ่งร้อยสิบเอ็ด"
+        )
+        self.assertEqual(
+            num2words(22222, lang='th'), "สองหมื่นสองพันสองร้อยยี่สิบสอง"
+        )
+        self.assertEqual(
+            num2words(84573, lang='th'), "แปดหมื่นสี่พันห้าร้อยเจ็ดสิบสาม"
+        )
+
+    def test_100000_to_999999(self):
+        self.assertEqual(
+            num2words(153247, lang='th'),
+            "หนึ่งแสนห้าหมื่นสามพันสองร้อยสี่สิบเจ็ด"
+        )
+        self.assertEqual(
+            num2words(562442, lang='th'),
+            "ห้าแสนหกหมื่นสองพันสี่ร้อยสี่สิบสอง"
+        )
+        self.assertEqual(
+            num2words(999999, lang='th'),
+            "เก้าแสนเก้าหมื่นเก้าพันเก้าร้อยเก้าสิบเก้า"
+        )
+
+    def test_more_than_million(self):
+        self.assertEqual(
+            num2words(1000000, lang='th'),
+            "หนึ่งล้าน"
+        )
+        self.assertEqual(
+            num2words(1000001, lang='th'),
+            "หนึ่งล้านเอ็ด"
+        )
+        self.assertEqual(
+            num2words(42478941, lang='th'),
+            "สี่สิบสองล้านสี่แสนเจ็ดหมื่นแปดพันเก้าร้อยสี่สิบเอ็ด"
+        )
+        self.assertEqual(
+            num2words(712696969, lang='th'),
+            "เจ็ดร้อยสิบสองล้านหกแสนเก้าหมื่นหกพันเก้าร้อยหกสิบเก้า"
+        )
+        self.assertEqual(
+            num2words(1000000000000000001, lang='th'),
+            "หนึ่งล้านล้านล้านเอ็ด"
+        )
+        self.assertEqual(
+            num2words(123001998830750501, lang='th'),
+            "หนึ่งแสนสองหมื่นสามพันเอ็ดล้านเก้าแสนเก้าหมื่นแปดพันแปดร้อยสามสิบล้านเจ็ดแสนห้าหมื่นห้าร้อยเอ็ด"
+        )
+
+    def test_decimal(self):
+        self.assertEqual(
+            num2words(0.0, lang='th'), "ศูนย์"
+        )
+        self.assertEqual(
+            num2words(0.0038, lang='th'), "ศูนย์จุดศูนย์ศูนย์สามแปด"
+        )
+        self.assertEqual(
+            num2words(0.01, lang='th'), "ศูนย์จุดศูนย์หนึ่ง"
+        )
+        self.assertEqual(
+            num2words(1.123, lang='th'), "หนึ่งจุดหนึ่งสองสาม"
+        )
+        self.assertEqual(
+            num2words(35.37, lang='th'), "สามสิบห้าจุดสามเจ็ด"
+        )
+        self.assertEqual(
+            num2words(1000000.01, lang='th'), "หนึ่งล้านจุดศูนย์หนึ่ง"
+        )
+
+    def test_currency_decimal(self):
+        self.assertEqual(
+            num2words(0.00, lang='th', to='currency'), "ศูนย์บาทถ้วน"
+        )
+        self.assertEqual(
+            num2words(0.05, lang='th', to='currency'), "ห้าสตางค์"
+        )
+        self.assertEqual(
+            num2words(0.50, lang='th', to='currency'), "ห้าสิบสตางค์"
+        )
+        self.assertEqual(
+            num2words(0.99, lang='th', to='currency'), "เก้าสิบเก้าสตางค์"
+        )
+        self.assertEqual(
+            num2words(100.00, lang='th', to='currency'), "หนึ่งร้อยบาทถ้วน"
+        )
+
+    def test_round_2_decimal(self):
+        self.assertEqual(round_2_decimal(0.004), '0.00')
+        self.assertEqual(round_2_decimal(0.005), '0.01')
+        self.assertEqual(round_2_decimal(0.006), '0.01')
+        self.assertEqual(round_2_decimal(0.0005), '0.00')
+        self.assertEqual(round_2_decimal(0.984), '0.98')
+        self.assertEqual(round_2_decimal(0.989), '0.99')
+        self.assertEqual(round_2_decimal(0.994), '0.99')
+        self.assertEqual(round_2_decimal(0.999), '1.00')
+        # self.assertEqual(round_2_decimal(0.985), '0.99')
+        # Expect 0.99 get 0.98
+        # self.assertEqual(round_2_decimal(0.995), '1.00')
+        # Expect 1.00 get 0.99
