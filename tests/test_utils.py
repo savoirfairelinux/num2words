b9020ca7771195b307e70ec2aa8fe8c6a7552003
pipeCh <pipech@gmail.com>
2017-12-01 21:41:56 +0700

    pep8

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,12 +7,14 @@ class TestUtils(TestCase):
     def test_splitbyx(self):
         self.assertEqual(list(splitbyx(str(12), 3)), [12])
         self.assertEqual(list(splitbyx(str(1234), 3)), [1, 234])
-        self.assertEqual(list(splitbyx(str(12345678900), 3)), [12, 345, 678, 900])
+        self.assertEqual(list(splitbyx(str(12345678900), 3)),
+                         [12, 345, 678, 900]
+                         )
         self.assertEqual(list(splitbyx(str(1000000), 6)), [1, 0])
 
         self.assertEqual(list(splitbyx(str(12), 3, format_int=False)), ['12'])
-        self.assertEqual(list(splitbyx(str(1234), 3, format_int=False))
-                         , ['1', '234']
+        self.assertEqual(list(splitbyx(str(1234), 3, format_int=False)),
+                         ['1', '234']
                          )
         self.assertEqual(list(splitbyx(str(12345678900), 3, format_int=False)),
                          ['12', '345', '678', '900']
