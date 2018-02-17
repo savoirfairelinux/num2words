0c4166ae64d0b608ffe71c59d22422066e91ef29
dede64 <danik.ridzon@seznam.cz>
2018-02-17 23:02:06 +0100

    Fixed tests

--- tests/test_cz.py
+++ tests/test_cz.py
@@ -41,25 +41,25 @@ class Num2WordsPLTest(TestCase):
         )
         self.assertEqual(
             num2words(1234567890, lang='cz'),
-            "miliarda dvěstě třicet čtyři miliony pětset šedesát"
+            "miliarda dvěstě třicet čtyři miliony pětset šedesát "
             "sedm tisíc osmset devadesát"
         )
         self.assertEqual(
             num2words(215461407892039002157189883901676, lang='cz'),
-            "dvěstě patnáct quintillionů čtyřista šedesát jedna kvadriliard"
-            "čtyřista sedm kvadrilionů osmset devadesát dva triliardy třicet"
-            "devět trilionů dva biliardy sto padesát sedm bilionů sto osmdesát"
-            "devět miliard osmset osmdesát tři miliony devětset jedna tisíc"
+            "dvěstě patnáct quintillionů čtyřista šedesát jedna kvadriliard "
+            "čtyřista sedm kvadrilionů osmset devadesát dva triliardy třicet "
+            "devět trilionů dva biliardy sto padesát sedm bilionů sto osmdesát "
+            "devět miliard osmset osmdesát tři miliony devětset jedna tisíc "
             "šestset sedmdesát šest"
         )
         self.assertEqual(
             num2words(719094234693663034822824384220291, lang='cz'),
-            "sedmset devatenáct quintillionů devadesát"
-            "čtyři kvadriliardy dvěstě třicet čtyři"
-            "kvadriliony šestset devadesát tři triliardy"
-            "šestset šedesát tři triliony třicet čtyři biliardy osmset"
-            "dvacet dva biliony osmset dvacet čtyři"
-            "miliardy třista osmdesát čtyři miliony dvěstě dvacet"
+            "sedmset devatenáct quintillionů devadesát "
+            "čtyři kvadriliardy dvěstě třicet čtyři "
+            "kvadriliony šestset devadesát tři triliardy "
+            "šestset šedesát tři triliony třicet čtyři biliardy osmset "
+            "dvacet dva biliony osmset dvacet čtyři "
+            "miliardy třista osmdesát čtyři miliony dvěstě dvacet "
             "tisíc dvěstě devadesát jedna"
         )
 
