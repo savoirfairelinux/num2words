9c5bb2b8ab9be5604aee4d7db81f27e76ad8e83d
dede64 <danik.ridzon@seznam.cz>
2018-02-17 22:50:22 +0100

    Created tests for czech language.

--- tests/test_cz.py
+++ tests/test_cz.py
@@ -23,90 +23,78 @@ from num2words import num2words
 
 class Num2WordsPLTest(TestCase):
     def test_cardinal(self):
-        self.assertEqual(num2words(100, lang='pl'), "sto")
-        self.assertEqual(num2words(101, lang='pl'), "sto jedna")
-        self.assertEqual(num2words(110, lang='pl'), "sto deset")
-        self.assertEqual(num2words(115, lang='pl'), "sto patnáct")
-        self.assertEqual(num2words(123, lang='pl'), "sto dvacet tři")
-        self.assertEqual(num2words(1000, lang='pl'), "tisíc")
-        self.assertEqual(num2words(1001, lang='pl'), "tisíc jedna")
-        self.assertEqual(num2words(2012, lang='pl'), "dwa tysiące dwanaście")
+        self.assertEqual(num2words(100, lang='cz'), "sto")
+        self.assertEqual(num2words(101, lang='cz'), "sto jedna")
+        self.assertEqual(num2words(110, lang='cz'), "sto deset")
+        self.assertEqual(num2words(115, lang='cz'), "sto patnáct")
+        self.assertEqual(num2words(123, lang='cz'), "sto dvacet tři")
+        self.assertEqual(num2words(1000, lang='cz'), "tisíc")
+        self.assertEqual(num2words(1001, lang='cz'), "tisíc jedna")
+        self.assertEqual(num2words(2012, lang='cz'), "dva tisíce dvanáct")
         self.assertEqual(
-            num2words(12519.85, lang='pl'),
-            "dwanaście tysięcy pięćset dziewiętnaście przecinek "
-            "osiemdziesiąt pięć"
+            num2words(12519.85, lang='cz'),
+            "dvanáct tisíc pětset devatenáct celá osmdesát pět"
         )
         self.assertEqual(
-            num2words(123.50, lang='pl'),
-            "sto dwadzieścia trzy przecinek pięć"
+            num2words(123.50, lang='cz'),
+            "sto dvacet tři celá pět"
         )
         self.assertEqual(
-            num2words(1234567890, lang='pl'),
-            "miliard dwieście trzydzieści cztery miliony pięćset "
-            "sześćdziesiąt siedem tysięcy osiemset dziewięćdzisiąt"
+            num2words(1234567890, lang='cz'),
+            "miliarda dvěstě třicet čtyři miliony pětset šedesát sedm tisíc osmset devadesát"
         )
         self.assertEqual(
-            num2words(215461407892039002157189883901676, lang='pl'),
-            "dwieście piętnaście kwintylionów czterysta sześćdziesiąt jeden "
-            "kwadryliardów czterysta siedem kwadrylionów osiemset "
-            "dziewięćdzisiąt dwa tryliardy trzydzieści dziewięć trylionów "
-            "dwa biliardy sto pięćdziesiąt siedem bilionów sto osiemdziesiąt "
-            "dziewięć miliardów osiemset osiemdziesiąt trzy miliony "
-            "dziewęćset jeden tysięcy sześćset siedemdziesiąt sześć"
+            num2words(215461407892039002157189883901676, lang='cz'),
+            "dvěstě patnáct quintillionů čtyřista šedesát jedna kvadriliard"
+            "čtyřista sedm kvadrilionů osmset devadesát dva triliardy třicet"
+            "devět trilionů dva biliardy sto padesát sedm bilionů sto osmdesát"
+            "devět miliard osmset osmdesát tři miliony devětset jedna tisíc šestset sedmdesát šest"
         )
         self.assertEqual(
-            num2words(719094234693663034822824384220291, lang='pl'),
-            "siedemset dziewiętnaście kwintylionów dziewięćdzisiąt cztery "
-            "kwadryliardy dwieście trzydzieści cztery kwadryliony sześćset "
-            "dziewięćdzisiąt trzy tryliardy sześćset sześćdziesiąt trzy "
-            "tryliony trzydzieści cztery biliardy osiemset dwadzieścia dwa "
-            "biliony osiemset dwadzieścia cztery miliardy trzysta "
-            "osiemdziesiąt cztery miliony dwieście dwadzieścia "
-            "tysięcy dwieście dziewięćdzisiąt jeden"
+            num2words(719094234693663034822824384220291, lang='cz'),
+            "sedmset devatenáct quintillionů devadesát čtyři kvadriliardy dvěstě"
+            "třicet čtyři kvadriliony šestset devadesát tři triliardy šestset šedesát tři"
+            "triliony třicet čtyři biliardy osmset dvacet dva biliony osmset dvacet čtyři"
+            "miliardy třista osmdesát čtyři miliony dvěstě dvacet tisíc dvěstě devadesát jedna"
         )
 
     def test_to_ordinal(self):
         # @TODO: implement to_ordinal
         with self.assertRaises(NotImplementedError):
-            num2words(1, lang='pl', to='ordinal')
+            num2words(1, lang='cz', to='ordinal')
 
     def test_currency(self):
         self.assertEqual(
-            num2words(1.0, lang='pl', to='currency', currency='EUR'),
-            "jeden euro, zero centów")
+            num2words(10.0, lang='cz', to='currency', currency='EUR'),
+            "deset euro, nula centů")
         self.assertEqual(
-            num2words(1.0, lang='pl', to='currency', currency='PLN'),
-            "jeden złoty, zero groszy")
+            num2words(1.0, lang='cz', to='currency', currency='CZK'),
+            "jedna koruna, nula haléřů")
         self.assertEqual(
-            num2words(1234.56, lang='pl', to='currency', currency='EUR'),
-            "tysiąc dwieście trzydzieści cztery euro, pięćdziesiąt sześć "
-            "centów"
+            num2words(1234.56, lang='cz', to='currency', currency='EUR'),
+            "tisíc dvěstě třicet čtyři euro, padesát šest centů")
+        self.assertEqual(
+            num2words(1234.56, lang='cz', to='currency', currency='CZK'),
+            "tisíc dvěstě třicet čtyři koruny, padesát šest haléřů")
+        self.assertEqual(
+            num2words(101.11, lang='cz', to='currency', currency='EUR',
+                      seperator=' a'),
+            "sto jedna euro a jedenáct centů")
+        self.assertEqual(
+            num2words(101.21, lang='cz', to='currency', currency='CZK',
+                      seperator=' a'),
+            "sto jedna korun a dvacet jedna haléřů"
         )
         self.assertEqual(
-            num2words(1234.56, lang='pl', to='currency', currency='PLN'),
-            "tysiąc dwieście trzydzieści cztery złote, pięćdziesiąt sześć "
-            "groszy"
+            num2words(-12519.85, lang='cz', to='currency', cents=False),
+            "mínus dvanáct tisíc pětset devatenáct euro, 85 centů"
         )
         self.assertEqual(
-            num2words(10111, lang='pl', to='currency', currency='EUR',
-                      seperator=' i'),
-            "sto jeden euro i jedenaście centów"
+            num2words(123.50, lang='cz', to='currency', currency='CZK',
+                      seperator=' a'),
+            "sto dvacet tři koruny a padesát haléřů"
         )
         self.assertEqual(
-            num2words(10121, lang='pl', to='currency', currency='PLN',
-                      seperator=' i'),
-            "sto jeden złotych i dwadzieścia jeden groszy"
-        )
-        self.assertEqual(
-            num2words(-1251985, lang='pl', to='currency', cents=False),
-            "minus dwanaście tysięcy pięćset dziewiętnaście euro, 85 centów"
-        )
-        self.assertEqual(
-            num2words(123.50, lang='pl', to='currency', currency='PLN',
-                      seperator=' i'),
-            "sto dwadzieścia trzy złote i pięćdziesiąt groszy"
-        )
-        self.assertEqual(
-            num2words(1950, lang='pl', to='currency', cents=False),
-            "dziewiętnaście euro, 50 centów"
+            num2words(19.50, lang='cz', to='currency', cents=False),
+            "devatenáct euro, 50 centů"
         )
