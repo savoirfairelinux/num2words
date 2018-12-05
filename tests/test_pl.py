9bd1bebb0eb3ea02be6181028aeeb0a795d891ff
Marek Madejski <marekmadejski@yandex.com>
2018-12-05 10:40:43 +0100

    Shortened one test number for PEP8 compliance - smaller number parts are already tested.

--- tests/test_pl.py
+++ tests/test_pl.py
@@ -70,19 +70,15 @@ class Num2WordsPLTest(TestCase):
         )
         self.assertEqual(
             num2words(
-                963301000001918264129471042047146102350812074235000101020000120324,
+                963301000001918264129471001047146102 * 10**30 + 1007,
                 lang='pl'
             ),
             "dziewięćset sześćdziesiąt trzy decyliardy trzysta jeden "
             "decylionów nonylion dziewięćset osiemnaście oktyliardów dwieście "
             "sześćdziesiąt cztery oktyliony sto dwadzieścia dziewięć "
             "septyliardów czterysta siedemdziesiąt jeden septylionów "
-            "czterdzieści dwa sekstyliardy czterdzieści siedem sekstylionów "
-            "sto czterdzieści sześć kwintyliardów sto dwa kwintyliony trzysta "
-            "pięćdziesiąt kwadryliardów osiemset dwanaście kwadrylionów "
-            "siedemdziesiąt cztery tryliardy dwieście trzydzieści pięć "
-            "trylionów sto jeden bilionów dwadzieścia miliardów sto "
-            "dwadzieścia tysięcy trzysta dwadzieścia cztery"
+            "sekstyliard czterdzieści siedem sekstylionów sto czterdzieści "
+            "sześć kwintyliardów sto dwa kwintyliony tysiąc siedem"
         )
 
     def test_to_ordinal(self):
