6b68d4b7d647ce72adda206b42a7f9df98787b9d
akki2825 <akhilesh.rocking@gmail.com>
2019-02-12 22:38:45 +0530

    change lang param

--- tests/test_kn.py
+++ tests/test_kn.py
@@ -5,11 +5,13 @@ from num2words import num2words
 
 class Num2WordsKNTest(TestCase):
     def test_numbers(self):
-        self.assertEqual(num2words(42, lang='hi'), "ನಲವತ್ತ್ ಎರಡು")
-        self.assertEqual(num2words(893, lang="hi"), "ಎಂಟು ನೂರು ತೊಂಬತ್ತ ಮೂರು")
-        self.assertEqual(num2words(1729, lang="hi"), "ಒಂದು ಸಾವಿರ ಏಳು ನೂರು ಇಪ್ಪತ್ತ್ಒಂಬತ್ತು")
+        self.assertEqual(num2words(42, lang="kn"), "ನಲವತ್ತ್ ಎರಡು")
+        self.assertEqual(num2words(893, lang="kn"), "ಎಂಟು ನೂರು ತೊಂಬತ್ತ ಮೂರು")
+        self.assertEqual(
+            num2words(1729, lang="kn"), "ಒಂದು ಸಾವಿರ ಏಳು ನೂರು ಇಪ್ಪತ್ತ್ಒಂಬತ್ತು"
+        )
 
     def test_ordinals(self):
-        self.assertEqual(num2words(2, lang='hi'), "ಎರಡನೇ")
-        self.assertEqual(num2words(5, lang='hi'), "ಐದನೇ")
-        self.assertEqual(num2words(16, lang='hi'), "ಹದಿನಾರನೇ")
+        self.assertEqual(num2words(2, lang="kn"), "ಎರಡನೇ")
+        self.assertEqual(num2words(5, lang="kn"), "ಐದನೇ")
+        self.assertEqual(num2words(16, lang="kn"), "ಹದಿನಾರನೇ")
