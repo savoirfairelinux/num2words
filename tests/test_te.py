b89acf67090ff827273af108d14c2f14f0c6c964
Sarah Beranek <sberanek@apptek.com>
2019-08-19 12:19:33 +0200

    Add Ordinal Numbers Test for Kannada.

--- tests/test_te.py
+++ tests/test_te.py
@@ -29,15 +29,28 @@ class Num2WordsTETest(TestCase):
                          u"ఒకటి వందల ముప్పై నాలుగు")
         self.assertEqual(num2words(54411, lang="te"),
                          u"యాభై నాలుగు వేయి నాలుగు వందల పదకొండు")
+        self.assertEqual(num2words(42, lang="te"), u"నలభై రెండు")
+        self.assertEqual(num2words(893, lang="te"),
+                         u"ఎనిమిది వందల తొంభై మూడు")
+        self.assertEqual(
+            num2words(1729, lang="te"), u"ఒకటి వేయి ఏడు వందల ఇరవై తొమ్మిది"
+        )
+        self.assertEqual(num2words(123, lang="te"), u"ఒకటి వందల ఇరవై మూడు")
+        self.assertEqual(num2words(32211, lang="te"),
+                         u"ముప్పై రెండు వేయి రెండు వందల పదకొండు")
 
     def test_cardinal_for_float_number(self):
         self.assertEqual(num2words(1.61803, lang="te"),
                          u"ఒకటి బిందువు  ఆరు ఒకటి ఎనిమిది సున్న మూడు")
         self.assertEqual(num2words(34.876, lang="te"),
                          u"ముప్పై నాలుగు బిందువు  ఎనిమిది ఏడు ఆరు")
+        self.assertEqual(num2words(3.14, lang="te"),
+                         u"మూడు బిందువు  ఒకటి నాలుగు")
 
     def test_ordinal(self):
         self.assertEqual(num2words(1, lang='te', to='ordinal'), u"ఒకటివ")
+        self.assertEqual(num2words(22, lang='te', to='ordinal'),
+                         u"ఇరవై రెండువ")
         self.assertEqual(num2words(23, lang='te', to='ordinal'),
                          u"ఇరవై మూడువ")
         self.assertEqual(num2words(12, lang='te', to='ordinal'), u"పన్నెండువ")
@@ -45,8 +58,11 @@ class Num2WordsTETest(TestCase):
                          u"ఒకటి వందల ముప్పైవ")
         self.assertEqual(num2words(1003, lang='te', to='ordinal'),
                          u"ఒకటి వేయిల మూడువ")
+        self.assertEqual(num2words(4, lang='te', to='ordinal'),
+                         u"నాలుగువ")
 
     def test_ordinal_num(self):
+        self.assertEqual(num2words(2, lang="te", to='ordinal_num'), u"2వ")
         self.assertEqual(num2words(3, lang="te", to='ordinal_num'), u"3వ")
         self.assertEqual(num2words(5, lang="te", to='ordinal_num'), u"5వ")
         self.assertEqual(num2words(16, lang="te", to='ordinal_num'), u"16వ")
