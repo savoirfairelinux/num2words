84ecc37fd3617bf128baa39c80cfc44337f72187
npujol <naivy.luna@gmail.com>
2019-10-08 16:18:13 +0200

    Correction orthography. Adjusting exception.

--- num2words/lang_ES.py
+++ num2words/lang_ES.py
@@ -22,10 +22,10 @@ from .lang_EU import Num2Word_EU
 
 class Num2Word_ES(Num2Word_EU):
     CURRENCY_FORMS = {
-        'EUR': (('euro', 'euros'), ('céntimo', 'céntimos')),
-        'ESP': (('peseta', 'pesetas'), ('céntimo', 'céntimos')),
-        'USD': (('dolar', 'dólares'), ('centavo', 'centavos')),
-        'PEN': (('sol', 'soles'), ('céntimo', 'céntimos')),
+        "EUR": (("euro", "euros"), ("céntimo", "céntimos")),
+        "ESP": (("peseta", "pesetas"), ("céntimo", "céntimos")),
+        "USD": (("dolar", "dólares"), ("centavo", "centavos")),
+        "PEN": (("sol", "soles"), ("céntimo", "céntimos")),
     }
 
     # //CHECK: Is this sufficient??
@@ -38,54 +38,86 @@ class Num2Word_ES(Num2Word_EU):
         self.negword = "menos "
         self.pointword = "punto"
         self.errmsg_nonnum = "Solo números pueden ser convertidos a palabras."
-        self.errmsg_toobig = (
-            "Numero muy grande para ser convertido a palabras."
-            )
+        self.errmsg_toobig = "Numero muy grande para ser convertido a palabras."
         self.gender_stem = "o"
         self.exclude_title = ["y", "menos", "punto"]
-        self.mid_numwords = [(1000, "mil"), (100, "cien"), (90, "noventa"),
-                             (80, "ochenta"), (70, "setenta"), (60, "sesenta"),
-                             (50, "cincuenta"), (40, "cuarenta"),
-                             (30, "treinta")]
-        self.low_numwords = ["veintinueve", "veintiocho", "veintisiete",
-                             "veintiséis", "veinticinco", "veinticuatro",
-                             "veintitrés", "veintidós", "veintiuno",
-                             "veinte", "diecinueve", "dieciocho", "diecisiete",
-                             "dieciseis", "quince", "catorce", "trece", "doce",
-                             "once", "diez", "nueve", "ocho", "siete", "seis",
-                             "cinco", "cuatro", "tres", "dos", "uno", "cero"]
-        self.ords = {1: "primer",
-                     2: "segund",
-                     3: "tercer",
-                     4: "cuart",
-                     5: "quint",
-                     6: "sext",
-                     7: "séptim",
-                     8: "octav",
-                     9: "noven",
-                     10: "décim",
-                     20: "vigésim",
-                     30: "trigésim",
-                     40: "quadragésim",
-                     50: "quincuagésim",
-                     60: "sexagésim",
-                     70: "septuagésim",
-                     80: "octogésim",
-                     90: "nonagésim",
-                     100: "centésim",
-                     200: "ducentésim",
-                     300: "tricentésim",
-                     400: "cuadrigentésim",
-                     500: "quingentésim",
-                     600: "sexcentésim",
-                     700: "septigentésim",
-                     800: "octigentésim",
-                     900: "noningentésim",
-                     1e3: "milésim",
-                     1e6: "millonésim",
-                     1e9: "billonésim",
-                     1e12: "trillonésim",
-                     1e15: "cuadrillonésim"}
+        self.mid_numwords = [
+            (1000, "mil"),
+            (100, "cien"),
+            (90, "noventa"),
+            (80, "ochenta"),
+            (70, "setenta"),
+            (60, "sesenta"),
+            (50, "cincuenta"),
+            (40, "cuarenta"),
+            (30, "treinta"),
+        ]
+        self.low_numwords = [
+            "veintinueve",
+            "veintiocho",
+            "veintisiete",
+            "veintiséis",
+            "veinticinco",
+            "veinticuatro",
+            "veintitrés",
+            "veintidós",
+            "veintiuno",
+            "veinte",
+            "diecinueve",
+            "dieciocho",
+            "diecisiete",
+            "dieciseis",
+            "quince",
+            "catorce",
+            "trece",
+            "doce",
+            "once",
+            "diez",
+            "nueve",
+            "ocho",
+            "siete",
+            "seis",
+            "cinco",
+            "cuatro",
+            "tres",
+            "dos",
+            "uno",
+            "cero",
+        ]
+        self.ords = {
+            1: "primer",
+            2: "segund",
+            3: "tercer",
+            4: "cuart",
+            5: "quint",
+            6: "sext",
+            7: "séptim",
+            8: "octav",
+            9: "noven",
+            10: "décim",
+            20: "vigésim",
+            30: "trigésim",
+            40: "quadragésim",
+            50: "quincuagésim",
+            60: "sexagésim",
+            70: "septuagésim",
+            80: "octogésim",
+            90: "nonagésim",
+            100: "centésim",
+            200: "ducentésim",
+            300: "tricentésim",
+            400: "cuadrigentésim",
+            500: "quingentésim",
+            600: "sexcentésim",
+            700: "septigentésim",
+            800: "octigentésim",
+            900: "noningentésim",
+            1e3: "milésim",
+            1e6: "millonésim",
+            1e9: "billonésim",
+            1e12: "trillonésim",
+            1e15: "cuadrillonésim",
+        }
 
     def merge(self, curr, next):
         ctext, cnum, ntext, nnum = curr + next
@@ -126,22 +158,25 @@ class Num2Word_ES(Num2Word_EU):
             elif value <= 10:
                 text = "%s%s" % (self.ords[value], self.gender_stem)
             elif value <= 12:
-                text = (
-                    "%s%s%s" % (self.ords[10], self.gender_stem,
-                                self.to_ordinal(value - 10))
-                        )
+                text = "%s%s%s" % (
+                    self.ords[10],
+                    self.gender_stem,
+                    self.to_ordinal(value - 10),
+                )
             elif value <= 100:
                 dec = (value // 10) * 10
-                text = (
-                    "%s%s %s" % (self.ords[dec], self.gender_stem,
-                                 self.to_ordinal(value - dec))
-                        )
+                text = "%s%s %s" % (
+                    self.ords[dec],
+                    self.gender_stem,
+                    self.to_ordinal(value - dec),
+                )
             elif value <= 1e3:
                 cen = (value // 100) * 100
-                text = (
-                    "%s%s %s" % (self.ords[cen], self.gender_stem,
-                                 self.to_ordinal(value - cen))
-                        )
+                text = "%s%s %s" % (
+                    self.ords[cen],
+                    self.gender_stem,
+                    self.to_ordinal(value - cen),
+                )
             elif value < 1e18:
                 # dec contains the following:
                 # [ 1e3,  1e6): 1e3
@@ -151,13 +186,13 @@ class Num2Word_ES(Num2Word_EU):
                 # [1e15, 1e18): 1e15
                 dec = 10 ** ((((len(str(int(value))) - 1) / 3 - 1) + 1) * 3)
                 part = int(float(value / dec) * dec)
-                cardinal = (
-                    self.to_cardinal(part / dec) if part / dec != 1 else ""
-                    )
-                text = (
-                    "%s%s%s %s" % (cardinal, self.ords[dec], self.gender_stem,
-                                   self.to_ordinal(value - part))
-                        )
+                cardinal = self.to_cardinal(part / dec) if part / dec != 1 else ""
+                text = "%s%s%s %s" % (
+                    cardinal,
+                    self.ords[dec],
+                    self.gender_stem,
+                    self.to_ordinal(value - part),
+                )
             else:
                 text = self.to_cardinal(value)
         except KeyError:
@@ -166,12 +201,22 @@ class Num2Word_ES(Num2Word_EU):
 
     def to_ordinal_num(self, value):
         self.verify_ordinal(value)
-        return "%s%s" % (value, "º" if self.gender_stem == 'o' else "ª")
+        return "%s%s" % (value, "º" if self.gender_stem == "o" else "ª")
 
-    def to_currency(self, val, currency='EUR', cents=True, separator=' con',
-                    adjective=False):
+    def to_currency(
+        self, val, currency="EUR", cents=True, separator=" con", adjective=False
+    ):
         result = super(Num2Word_ES, self).to_currency(
-            val, currency=currency, cents=cents, separator=separator,
-            adjective=adjective)
+            val,
+            currency=currency,
+            cents=cents,
+            separator=separator,
+            adjective=adjective,
+        )
         # Handle exception, in spanish is "un euro" and not "uno euro"
-        return result.replace("uno", "un")
+        return (
+            result.replace("uno", "un")
+            if result.find("veintiuno")
+            else result.replace("veintiuno", "veintiún")
+        )
+
