ea1b8258cae83caf4ba642a3bd633bf2f474525a
Azim <yusoffab@users.noreply.github.com>
2019-11-09 12:57:30 +0800

    Add support for Malay

--- num2words/__init__.py
+++ num2words/__init__.py
@@ -20,8 +20,8 @@ from __future__ import unicode_literals
 from . import (lang_AR, lang_CZ, lang_DE, lang_DK, lang_EN, lang_EN_IN,
                lang_ES, lang_ES_CO, lang_ES_NI, lang_ES_VE, lang_FI, lang_FR,
                lang_FR_BE, lang_FR_CH, lang_FR_DZ, lang_HE, lang_ID, lang_IT,
-               lang_JA, lang_KN, lang_KO, lang_KZ, lang_LT, lang_LV, lang_NL,
-               lang_NO, lang_PL, lang_PT, lang_PT_BR, lang_RO, lang_RU,
+               lang_JA, lang_KN, lang_KO, lang_KZ, lang_LT, lang_LV, lang_MS,
+               lang_NL, lang_NO, lang_PL, lang_PT, lang_PT_BR, lang_RO, lang_RU,
                lang_SL, lang_SR, lang_TE, lang_TH, lang_TR, lang_UK, lang_VI)
 
 CONVERTER_CLASSES = {
@@ -46,6 +46,7 @@ CONVERTER_CLASSES = {
     'kz': lang_KZ.Num2Word_KZ(),
     'lt': lang_LT.Num2Word_LT(),
     'lv': lang_LV.Num2Word_LV(),
+    'ms': lang_MS.Num2Word_MS(),
     'pl': lang_PL.Num2Word_PL(),
     'ro': lang_RO.Num2Word_RO(),
     'ru': lang_RU.Num2Word_RU(),
