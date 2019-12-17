0fe36663b91b228283b3ba1d4ce7bbdc44a2286c
Artem Rizhov <artem.rizhov@gmail.com>
2019-12-17 04:43:11 +0200

    Fix the feminine currency processing for UAH

--- num2words/lang_UA.py
+++ num2words/lang_UA.py
@@ -198,8 +198,11 @@ class Num2Word_UA(Num2Word_Base):
         outwords[-1] = self.title(lastword)
         return " ".join(outwords).strip()
 
-    def _cents_verbose(self, number, currency):
+    def _money_verbose(self, number, currency):
         return self._int2word(number, currency == 'UAH')
+
+    def _cents_verbose(self, number, currency):
+        return self._int2word(number, currency in ('UAH', 'RUB'))
 
     def _int2word(self, n, feminine=False):
         if n < 0:
