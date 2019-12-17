d3b5b7af5370f298013911ed6e6a095cf40a8832
Artem Rizhov <artem.rizhov@gmail.com>
2019-12-17 03:32:56 +0200

    Register Ukrainian language with 'ua' code.

--- num2words/__init__.py
+++ num2words/__init__.py
@@ -22,7 +22,7 @@ from . import (lang_AR, lang_CZ, lang_DE, lang_DK, lang_EN, lang_EN_IN,
                lang_FR_BE, lang_FR_CH, lang_FR_DZ, lang_HE, lang_ID, lang_IT,
                lang_JA, lang_KN, lang_KO, lang_LT, lang_LV, lang_NL, lang_NO,
                lang_PL, lang_PT, lang_PT_BR, lang_RO, lang_RU, lang_SL,
-               lang_SR, lang_TH, lang_TR, lang_UK, lang_VI)
+               lang_SR, lang_TH, lang_TR, lang_UK, lang_VI, lang_UA)
 
 CONVERTER_CLASSES = {
     'ar': lang_AR.Num2Word_AR(),
@@ -60,7 +60,8 @@ CONVERTER_CLASSES = {
     'th': lang_TH.Num2Word_TH(),
     'tr': lang_TR.Num2Word_TR(),
     'nl': lang_NL.Num2Word_NL(),
-    'uk': lang_UK.Num2Word_UK()
+    'uk': lang_UK.Num2Word_UK(),
+    'ua': lang_UA.Num2Word_UA(),
 }
 
 
