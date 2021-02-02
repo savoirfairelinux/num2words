7e1c7ed9aae48077c304469eb92f2af27420c1d5
Virginie <virginie@recapp.ch>
2021-02-02 10:38:56 +0100

    adding rumantsh surmiran

---
+++ num2words/lang_RM_SURMIRAN.py
@@ -0,0 +1,245 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Virginie Holm, recapp IT AG. All Rights Reserved.
+# Based on lang_IT template from Filippo Costa.
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301 USA
+
+from __future__ import unicode_literals
+
+# Globals
+# -------
+
+ZERO = "nolla"
+
+CARDINAL_WORDS = [
+    ZERO, "en", "dus", "treis", "quatter", "tschintg", "seis", "set", "otg",
+    "nov", "diesch", "endesch", "dodesch", "tredesch", "quittordesch", "quindesch",
+    "sedesch", "dischset", "dischdotg", "dischnov"
+]
+
+ORDINAL_WORDS = [
+    ZERO, "amprem", "sagond", "terz", "quart", "tschintgavel", "seisavel",
+    "settavel", "otgavel", "novavel", "dieschavel", "endeschavel", "dodeschavel",
+    "tredeschavel", "quittordeschavel", "quindeschavel", "sedeschavel",
+    "dischsettavel", "dischdotgavel", "dischnovavel", "vantgavel"
+]
+
+# "20" = "ventg"
+STR_TENS = {2: "vantga", 3: "trenta", 4: "curanta", 5: "tschuncanta",
+            6: "sessanta", 7: "settanta", 8: "otganta", 9: "novanta"}
+
+# These prefixes are used for extremely big numbers.
+EXPONENT_PREFIXES = [
+    ZERO, "m", "b", "tr", "quadr", "quint", "sest", "sett", "ott", "nov", "dec"
+]
+
+
+# Utils
+# =====
+
+def phonetic_contraction(string):
+    ''' _ is a marker for "empty", i.e. no following unit
+
+    NOTE: two forms of 1 exist (cardinal 'en' and pronoun 'egn') - the form 'egn'
+    is used within complex numbers, i.e. > 20
+    '''
+    return (string
+            .replace("aen", "egn")   # ex. "trentaen" -> "trentegn"
+            .replace("aotg", "otg")  # ex. "curantaotg" -> "curantotg"
+            .replace("tga_", "tg")   # ex. "vantga" -> "vantg"
+            .replace("_", "")
+            )
+
+def adapt_hundred(string):
+    '''apply surface modifications:
+         - 2: dus -> du, 3: treis -> tre
+         - a/ad phonotactic adaptation
+    '''
+    return (string
+            .replace("dustschent", "dutschent")
+            .replace("treistschent", "tretschent")
+            .replace("aendesch", "adendesch")
+            .replace("aen", "adegn")
+            .replace("aotg", "adotg")
+            )
+
+def adapt_thousand(string):
+    '''apply surface modifications:
+         - 2: dus -> du, 3: treis -> tre
+         - a/ad phonotactic adaptation
+    '''
+    return (string
+            .replace("dusmella", "dumella")
+            .replace("treismella", "tremella")
+            .replace("aendesch", "adendesch")
+            .replace("aaen", "adegn")
+            .replace("aaotg", "adotg")
+            .replace("aa", "a")
+            )
+
+def adapt_milliarda(string):
+    '''apply surface modifications:
+         - article gender agreement
+         - e/ed phonotactic adaptation
+    '''
+    string = " " + string + " "
+    return (string
+            .replace(" en milliarda ", " ena milliarda ")
+            .replace(" a en", " ad egn")               # check if 'e' or 'a'
+            .replace(" a otg", " ad otg")
+            )
+
+def exponent_length_to_string(exponent_length):
+    # We always assume `exponent` to be a multiple of 3. If it's not true, then
+    # Num2Word_RM_SURMIRAN.big_number_to_cardinal did something wrong.
+    prefix = EXPONENT_PREFIXES[exponent_length // 6]
+    if exponent_length % 6 == 0:
+        return prefix + "illiun"
+    else:
+        return prefix + "illiarda"
+
+def omitt_if_zero(number_to_string):
+    return "" if number_to_string == ZERO else number_to_string
+
+def empty_if_zero(number_to_string):
+    return "_" if number_to_string == ZERO else number_to_string
+
+
+# Main class
+# ==========
+
+class Num2Word_RM_SURMIRAN:
+    MINUS_PREFIX_WORD = "minus "
+    FLOAT_INFIX_WORD = " comma "
+
+    def __init__(self):
+        pass
+
+    def float_to_words(self, float_number, ordinal=False):
+        if ordinal:
+            prefix = self.to_ordinal(int(float_number))
+        else:
+            prefix = self.to_cardinal(int(float_number))
+        float_part = str(float_number).split('.')[1]
+        postfix = " ".join(
+            # Drops the trailing zero and comma
+            [self.to_cardinal(int(c)) for c in float_part]
+        )
+        return prefix + Num2Word_RM_SURMIRAN.FLOAT_INFIX_WORD + postfix
+
+    def tens_to_cardinal(self, number):
+        tens = number // 10
+        units = number % 10
+        if tens in STR_TENS:
+            prefix = STR_TENS[tens]
+        else:
+            prefix = CARDINAL_WORDS[tens][:-1] + "anta"
+        # we keep track of 0 using '_' -- removed in phonetic_contraction
+        postfix = empty_if_zero(CARDINAL_WORDS[units])
+        return phonetic_contraction(prefix + postfix)
+
+    def hundreds_to_cardinal(self, number):
+        hundreds = number // 100
+        tens = number % 100
+        prefix = "tschent"
+        if hundreds != 1:
+            prefix = CARDINAL_WORDS[hundreds] + prefix
+        postfix = omitt_if_zero(self.to_cardinal(tens))
+        # "a/ad" is inserted if tens <= 13 or = 15, 16, 20, 30
+        # distribution may seem unusual but it was reviewed by a native speaker
+        # surmiran's "and" is normally "e/ed", but in numbers, "a/ad" is used
+        infix = ""
+        if (tens > 0 and tens <= 13) or tens in [15, 16, 20, 30]:
+            infix = "a"                                   #check
+        return adapt_hundred(prefix + infix + postfix)
+
+    def thousands_to_cardinal(self, number):
+        thousands = number // 1000
+        hundreds = number % 1000
+        prefix = "mella"
+        if thousands != 1:
+            prefix = self.to_cardinal(thousands) + "mella"
+        postfix = omitt_if_zero(self.to_cardinal(hundreds))
+        # "e/ed" is inserted if tens <= 100               #check
+        infix = ""
+        if hundreds <= 100 and postfix != "":
+            infix = "a"                                   #check
+        return adapt_thousand(prefix + infix + postfix)
+
+    def big_number_to_cardinal(self, number):
+        digits = [c for c in str(number)]
+        length = len(digits)
+        if length >= 66:
+            raise NotImplementedError("The given number is too large.")
+        # This is how many digits come before the "illion" term.
+        #   tschent milliardas => 3
+        #   diesch milliuns => 2
+        #   ena milliarda => 1
+        predigits = length % 3 or 3
+        multiplier = digits[:predigits]
+        exponent = digits[predigits:]
+        infix = exponent_length_to_string(len(exponent))
+        if multiplier == ["1"]:
+            prefix = "en "
+        else:
+            prefix = self.to_cardinal(int("".join(multiplier)))
+            # Plural form
+            infix = " " + infix + "s"
+        # Read as: Does the value of exponent equal 0?
+        if set(exponent) != set("0"):
+            exponent_str = "".join(exponent)
+            postfix = self.to_cardinal(int(exponent_str))
+            # we introduce "e" if 3-digits gap before next value
+            if exponent_str.startswith('000'):
+                infix += " a "
+            else:
+                infix += " "
+        else:
+            postfix = ""
+        return adapt_milliarda(prefix + infix + postfix).strip()
+
+    def to_cardinal(self, number):
+        if number < 0:
+            string = Num2Word_RM_SURMIRAN.MINUS_PREFIX_WORD + self.to_cardinal(-number)
+        elif isinstance(number, float):
+            string = self.float_to_words(number)
+        elif number < 20:
+            string = CARDINAL_WORDS[number]
+        elif number < 100:
+            string = self.tens_to_cardinal(number)
+        elif number < 1000:
+            string = self.hundreds_to_cardinal(number)
+        elif number < 1000000:
+            string = self.thousands_to_cardinal(number)
+        else:
+            string = self.big_number_to_cardinal(number)
+        return string
+
+    def to_ordinal(self, number):
+        tens = number % 100
+        if number < 0:
+            return Num2Word_RM_SURMIRAN.MINUS_PREFIX_WORD + self.to_ordinal(-number)
+        elif number % 1 != 0:
+            return self.float_to_words(number, ordinal=True)
+        elif number <= 20:
+            return ORDINAL_WORDS[number]
+        else:
+            cardinal = self.to_cardinal(number)
+            if cardinal[-1] == 'a':
+                suffix = 'vel'
+            elif cardinal.endswith('set'):
+                suffix = 'tavel'
+            else:
+                suffix = 'avel'
+            return cardinal + suffix
---
+++ tests/test_rm_surmiran.py
@@ -0,0 +1,312 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2003, Taro Ogawa.  All Rights Reserved.
+# Copyright (c) 2013, Savoir-faire Linux inc.  All Rights Reserved.
+# Copyright (c) 2021, Virginie Holm, recapp IT AG.  All Rights Reserved.
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301 USA
+
+from __future__ import unicode_literals
+
+from unittest import TestCase
+
+from num2words import num2words
+
+
+class Num2WordsRMSURMIRANTest(TestCase):
+    maxDiff = None
+
+    def test_negative(self):
+        number = 648972145
+        pos_crd = num2words(+number, lang="rm_surmiran")
+        neg_crd = num2words(-number, lang="rm_surmiran")
+        pos_ord = num2words(+number, lang="rm_surmiran", ordinal=True)
+        neg_ord = num2words(-number, lang="rm_surmiran", ordinal=True)
+        self.assertEqual("minus " + pos_crd, neg_crd)
+        self.assertEqual("minus " + pos_ord, neg_ord)
+
+    def test_float_to_cardinal(self):
+        self.assertEqual(
+            num2words(3.1415, lang="rm_surmiran"), "treis comma en quatter en tschintg"
+        )
+        self.assertEqual(
+            num2words(-5.15, lang="rm_surmiran"), "minus tschintg comma en tschintg"
+        )
+        self.assertEqual(
+            num2words(-0.15, lang="rm_surmiran"), "minus nolla comma en tschintg"
+        )
+
+    def test_float_to_ordinal(self):
+        self.assertEqual(
+            num2words(3.1415, lang="rm_surmiran", ordinal=True),
+            "terz comma en quatter en tschintg"
+        )
+        self.assertEqual(
+            num2words(-5.15, lang="rm_surmiran", ordinal=True),
+            "minus tschintgavel comma en tschintg"
+        )
+        self.assertEqual(
+            num2words(-0.15, lang="rm_surmiran", ordinal=True),
+            "minus nolla comma en tschintg"
+        )
+
+    def test_0(self):
+        self.assertEqual(num2words(0, lang="rm_surmiran"), "nolla")
+        self.assertEqual(num2words(0, lang="rm_surmiran", ordinal=True), "nolla")
+
+    def test_1_to_10(self):
+        self.assertEqual(num2words(1, lang="rm_surmiran"), "en")
+        self.assertEqual(num2words(2, lang="rm_surmiran"), "dus")
+        self.assertEqual(num2words(3, lang="rm_surmiran"), "treis")
+        self.assertEqual(num2words(5, lang="rm_surmiran"), "tschintg")
+        self.assertEqual(num2words(7, lang="rm_surmiran"), "set")
+        self.assertEqual(num2words(8, lang="rm_surmiran"), "otg")
+        self.assertEqual(num2words(10, lang="rm_surmiran"), "diesch")
+
+    def test_11_to_19(self):
+        self.assertEqual(num2words(11, lang="rm_surmiran"), "endesch")
+        self.assertEqual(num2words(13, lang="rm_surmiran"), "tredesch")
+        self.assertEqual(num2words(15, lang="rm_surmiran"), "quindesch")
+        self.assertEqual(num2words(16, lang="rm_surmiran"), "sedesch")
+        self.assertEqual(num2words(17, lang="rm_surmiran"), "dischset")
+        self.assertEqual(num2words(18, lang="rm_surmiran"), "dischdotg")
+        self.assertEqual(num2words(19, lang="rm_surmiran"), "dischnov")
+
+    def test_20_to_99(self):
+        self.assertEqual(num2words(20, lang="rm_surmiran"), "vantg")
+        self.assertEqual(num2words(21, lang="rm_surmiran"), "vantgegn")
+        self.assertEqual(num2words(22, lang="rm_surmiran"), "vantgadus")
+        self.assertEqual(num2words(23, lang="rm_surmiran"), "vantgatreis")
+        self.assertEqual(num2words(28, lang="rm_surmiran"), "vantgotg")
+        self.assertEqual(num2words(30, lang="rm_surmiran"), "trenta")
+        self.assertEqual(num2words(31, lang="rm_surmiran"), "trentegn")
+        self.assertEqual(num2words(34, lang="rm_surmiran"), "trentaquatter")
+        self.assertEqual(num2words(38, lang="rm_surmiran"), "trentotg")
+        self.assertEqual(num2words(40, lang="rm_surmiran"), "curanta")
+        self.assertEqual(num2words(50, lang="rm_surmiran"), "tschuncanta")
+        self.assertEqual(num2words(59, lang="rm_surmiran"), "tschuncantanov")
+        self.assertEqual(num2words(66, lang="rm_surmiran"), "sessantaseis")
+        self.assertEqual(num2words(77, lang="rm_surmiran"), "settantaset")
+        self.assertEqual(num2words(81, lang="rm_surmiran"), "otgantegn")
+        self.assertEqual(num2words(92, lang="rm_surmiran"), "novantadus")
+
+    def test_100_to_999(self):
+        self.assertEqual(num2words(100, lang="rm_surmiran"), "tschent")
+        self.assertEqual(num2words(101, lang="rm_surmiran"), "tschentadegn")
+        self.assertEqual(num2words(102, lang="rm_surmiran"), "tschentadus")
+        self.assertEqual(num2words(103, lang="rm_surmiran"), "tschentatreis")
+        self.assertEqual(num2words(104, lang="rm_surmiran"), "tschentaquatter")
+        self.assertEqual(num2words(105, lang="rm_surmiran"), "tschentatschintg")
+        self.assertEqual(num2words(106, lang="rm_surmiran"), "tschentaseis")
+        self.assertEqual(num2words(107, lang="rm_surmiran"), "tschentaset")
+        self.assertEqual(num2words(108, lang="rm_surmiran"), "tschentadotg")
+        self.assertEqual(num2words(109, lang="rm_surmiran"), "tschentanov")
+        self.assertEqual(num2words(110, lang="rm_surmiran"), "tschentadiesch")
+        self.assertEqual(num2words(111, lang="rm_surmiran"), "tschentadendesch")
+
+        self.assertEqual(num2words(112, lang="rm_surmiran"), "tschentadodesch")
+
+        self.assertEqual(num2words(113, lang="rm_surmiran"), "tschentatredesch")
+
+        self.assertEqual(num2words(114, lang="rm_surmiran"), "tschentquittordesch")
+        self.assertEqual(num2words(115, lang="rm_surmiran"), "tschentaquindesch")
+        self.assertEqual(num2words(116, lang="rm_surmiran"), "tschentasedesch")
+        self.assertEqual(num2words(117, lang="rm_surmiran"), "tschentdischset")
+        self.assertEqual(num2words(118, lang="rm_surmiran"), "tschentdischdotg")
+        self.assertEqual(num2words(119, lang="rm_surmiran"), "tschentdischnov")
+        self.assertEqual(num2words(120, lang="rm_surmiran"), "tschentavantg")
+
+        self.assertEqual(num2words(121, lang="rm_surmiran"), "tschentvantgegn")
+        self.assertEqual(num2words(122, lang="rm_surmiran"), "tschentvantgadus")
+        self.assertEqual(num2words(123, lang="rm_surmiran"), "tschentvantgatreis")
+        self.assertEqual(num2words(124, lang="rm_surmiran"), "tschentvantgaquatter")
+        self.assertEqual(num2words(125, lang="rm_surmiran"), "tschentvantgatschintg")
+        self.assertEqual(num2words(126, lang="rm_surmiran"), "tschentvantgaseis")
+        self.assertEqual(num2words(127, lang="rm_surmiran"), "tschentvantgaset")
+        self.assertEqual(num2words(128, lang="rm_surmiran"), "tschentvantgotg")
+        self.assertEqual(num2words(129, lang="rm_surmiran"), "tschentvantganov")
+
+        self.assertEqual(num2words(130, lang="rm_surmiran"), "tschentatrenta")
+        self.assertEqual(num2words(131, lang="rm_surmiran"), "tschenttrentegn")
+        self.assertEqual(num2words(150, lang="rm_surmiran"), "tschenttschuncanta")
+        self.assertEqual(num2words(196, lang="rm_surmiran"), "tschentnovantaseis")
+        self.assertEqual(num2words(200, lang="rm_surmiran"), "dutschent")
+        self.assertEqual(num2words(208, lang="rm_surmiran"), "dutschentadotg")
+        self.assertEqual(num2words(210, lang="rm_surmiran"), "dutschentadiesch")
+        self.assertEqual(num2words(271, lang="rm_surmiran"), "dutschentsettantegn")
+        self.assertEqual(num2words(300, lang="rm_surmiran"), "tretschent")
+        self.assertEqual(num2words(308, lang="rm_surmiran"), "tretschentadotg")
+        self.assertEqual(num2words(311, lang="rm_surmiran"), "tretschentadendesch")
+        self.assertEqual(
+            num2words(375, lang="rm_surmiran"), "tretschentsettantatschintg"
+        )
+        self.assertEqual(num2words(400, lang="rm_surmiran"), "quattertschent")
+        self.assertEqual(num2words(409, lang="rm_surmiran"), "quattertschentanov")
+        self.assertEqual(num2words(410, lang="rm_surmiran"), "quattertschentadiesch")
+        self.assertEqual(num2words(472, lang="rm_surmiran"), "quattertschentsettantadus")
+        self.assertEqual(num2words(701, lang="rm_surmiran"), "settschentadegn")
+
+    def test_1000_to_9999(self):
+        self.assertEqual(num2words(1000, lang="rm_surmiran"), "mella")
+        self.assertEqual(num2words(1001, lang="rm_surmiran"), "melladegn")
+        self.assertEqual(num2words(1010, lang="rm_surmiran"), "melladiesch")
+        self.assertEqual(num2words(1100, lang="rm_surmiran"), "mellatschent")
+        self.assertEqual(num2words(1101, lang="rm_surmiran"), "mellatschentadegn")
+        self.assertEqual(num2words(1132, lang="rm_surmiran"), "mellatschenttrentadus")
+        self.assertEqual(num2words(1500, lang="rm_surmiran"), "mellatschintgtschent")
+        self.assertEqual(
+            num2words(7378, lang="rm_surmiran"), "setmellatretschentsettantotg"
+        )
+        self.assertEqual(num2words(2000, lang="rm_surmiran"), "dumella")
+        self.assertEqual(num2words(2001, lang="rm_surmiran"), "dumelladegn")
+        self.assertEqual(num2words(2020, lang="rm_surmiran"), "dumellavantg")
+        self.assertEqual(num2words(2100, lang="rm_surmiran"), "dumellatschent")
+        self.assertEqual(num2words(2101, lang="rm_surmiran"), "dumellatschentadegn")
+        self.assertEqual(num2words(3000, lang="rm_surmiran"), "tremella")
+        self.assertEqual(num2words(3012, lang="rm_surmiran"), "tremelladodesch")
+        self.assertEqual(
+            num2words(6870, lang="rm_surmiran"), "seismellaotgtschentsettanta"
+        )
+        self.assertEqual(num2words(10000, lang="rm_surmiran"), "dieschmella")
+        self.assertEqual(num2words(10001, lang="rm_surmiran"), "dieschmelladegn")
+        self.assertEqual(
+            num2words(98765, lang="rm_surmiran"),
+            "novantotgmellasettschentsessantatschintg"
+        )
+        self.assertEqual(num2words(100000, lang="rm_surmiran"), "tschentmella")
+        self.assertEqual(
+            num2words(523456, lang="rm_surmiran"),
+            "tschintgtschentvantgatremellaquattertschenttschuncantaseis"
+        )
+
+    def test_big(self):
+        self.assertEqual(num2words(1000000, lang="rm_surmiran"), "en milliun")
+        self.assertEqual(num2words(1000007, lang="rm_surmiran"), "en milliun a set")
+        self.assertEqual(num2words(1000008, lang="rm_surmiran"), "en milliun ad otg")
+        self.assertEqual(
+            num2words(1200000, lang="rm_surmiran"), "en milliun dutschentmella"
+        )
+        self.assertEqual(num2words(2000000, lang="rm_surmiran"), "dus milliuns")
+        self.assertEqual(num2words(2000004, lang="rm_surmiran"), "dus milliuns a quatter")
+        self.assertEqual(num2words(2000009, lang="rm_surmiran"), "dus milliuns a nov")
+        self.assertEqual(
+            num2words(2200311, lang="rm_surmiran"), "dus milliuns dutschentmellatretschentadendesch"
+        )
+        self.assertEqual(
+            num2words(2300000, lang="rm_surmiran"), "dus milliuns tretschentmella"
+        )
+        self.assertEqual(num2words(3000000, lang="rm_surmiran"), "treis milliuns")
+        self.assertEqual(
+            num2words(3000005, lang="rm_surmiran"), "treis milliuns a tschintg"
+        )
+        self.assertEqual(
+            num2words(3800000, lang="rm_surmiran"), "treis milliuns otgtschentmella"
+        )
+        self.assertEqual(num2words(1000000000, lang="rm_surmiran"), "ena milliarda")
+        self.assertEqual(
+            num2words(1000000017, lang="rm_surmiran"), "ena milliarda a dischset"
+        )
+        self.assertEqual(num2words(2000000000, lang="rm_surmiran"), "dus milliardas")
+        self.assertEqual(
+            num2words(2000001000, lang="rm_surmiran"), "dus milliardas a mella"
+        )
+        self.assertEqual(
+            num2words(3000000100, lang="rm_surmiran"), "treis milliardas a tschent"
+        )
+        self.assertEqual(
+            num2words(3000002000, lang="rm_surmiran"), "treis milliardas a dumella"
+        )
+        self.assertEqual(
+            num2words(3002000100, lang="rm_surmiran"), "treis milliardas dus milliuns a tschent"
+        )
+        self.assertEqual(
+            num2words(3002000101, lang="rm_surmiran"), "treis milliardas dus milliuns a tschentadegn"
+        )
+        self.assertEqual(num2words(21000000000, lang="rm_surmiran"), "vantgegn milliardas")
+        self.assertEqual(num2words(22000000000, lang="rm_surmiran"), "vantgadus milliardas")
+        self.assertEqual(
+            num2words(1234567890, lang="rm_surmiran"),
+            "ena milliarda dutschenttrentaquatter milliuns "
+            "tschintgtschentsessantasetmellaotgtschentnovanta"
+        )
+        self.assertEqual(num2words(1000000000000, lang="rm_surmiran"), "en billiun")
+        self.assertEqual(
+            num2words(123456789012345678901234567890, lang="rm_surmiran"),
+            "tschentvantgatreis quadrilliardas quattertschenttschuncantaseis"
+            " quadrilliuns settschentotgantanov trilliardas dodesch "
+            "trilliuns tretschentcurantatschintg billiardas "
+            "seistschentsettantotg billiuns novtschentadegn milliardas "
+            "dutschenttrentaquatter milliuns "
+            "tschintgtschentsessantasetmellaotgtschentnovanta"
+        )
+
+    def test_nth_1_to_99(self):
+        self.assertEqual(num2words(1, lang="rm_surmiran", ordinal=True), "amprem")
+        self.assertEqual(num2words(7, lang="rm_surmiran", ordinal=True), "settavel")
+        self.assertEqual(num2words(8, lang="rm_surmiran", ordinal=True), "otgavel")
+        self.assertEqual(num2words(20, lang="rm_surmiran", ordinal=True), "vantgavel")
+        self.assertEqual(num2words(21, lang="rm_surmiran", ordinal=True), "vantgegnavel")
+        self.assertEqual(
+            num2words(27, lang="rm_surmiran", ordinal=True), "vantgasettavel"
+        )
+        self.assertEqual(
+            num2words(48, lang="rm_surmiran", ordinal=True), "curantotgavel"
+        )
+        self.assertEqual(num2words(60, lang="rm_surmiran", ordinal=True), "sessantavel")
+        self.assertEqual(
+            num2words(99, lang="rm_surmiran", ordinal=True), "novantanovavel"
+        )
+
+    def test_nth_100_to_999(self):
+        self.assertEqual(num2words(100, lang="rm_surmiran", ordinal=True), "tschentavel")
+        self.assertEqual(
+            num2words(112, lang="rm_surmiran", ordinal=True), "tschentadodeschavel"
+        )
+        self.assertEqual(
+            num2words(137, lang="rm_surmiran", ordinal=True), "tschenttrentasettavel"
+        )
+        self.assertEqual(
+            num2words(700, lang="rm_surmiran", ordinal=True), "settschentavel"
+        )
+
+    def test_nth_1000_to_999999(self):
+        self.assertEqual(num2words(1000, lang="rm_surmiran", ordinal=True), "mellavel")
+        self.assertEqual(
+            num2words(1001, lang="rm_surmiran", ordinal=True), "melladegnavel"
+        )
+        self.assertEqual(
+            num2words(1200, lang="rm_surmiran", ordinal=True), "melladutschentavel"
+        )
+        self.assertEqual(
+            num2words(8640, lang="rm_surmiran", ordinal=True),
+            "otgmellaseistschentcurantavel"
+        )
+        self.assertEqual(
+            num2words(14000, lang="rm_surmiran", ordinal=True), "quittordeschmellavel"
+        )
+        self.assertEqual(
+            num2words(123456, lang="rm_surmiran", ordinal=True),
+            "tschentvantgatremellaquattertschenttschuncantaseisavel"
+        )
+        self.assertEqual(
+            num2words(987655, lang="rm_surmiran", ordinal=True),
+            "novtschentotgantasetmellaseistschenttschuncantatschintgavel"
+        )
+
+    def test_with_decimals(self):
+        self.assertAlmostEqual(
+            num2words(1.0, lang="rm_surmiran"), "en comma nolla"
+        )
+        self.assertAlmostEqual(
+            num2words(1.1, lang="rm_surmiran"), "en comma en"
+        )
