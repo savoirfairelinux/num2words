06ac538404aab6778a5e2c60a5235a7523600d02
punit121 <punit.kumar1661@gmail.com>
2021-05-01 17:30:12 +0530

    hi added in init

--- num2words/__init__.py
+++ num2words/__init__.py
@@ -23,7 +23,7 @@ from . import (lang_AR, lang_CZ, lang_DE, lang_DK, lang_EN, lang_EN_IN,
                lang_IT, lang_JA, lang_KN, lang_KO, lang_KZ, lang_LT, lang_LV,
                lang_NL, lang_NO, lang_PL, lang_PT, lang_PT_BR, lang_RO,
                lang_RU, lang_SL, lang_SR, lang_TE, lang_TH, lang_TR, lang_UK,
-               lang_VI)
+               lang_VI, lang_HI, lang_HI_EN)
 
 CONVERTER_CLASSES = {
     'ar': lang_AR.Num2Word_AR(),
