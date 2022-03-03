8baad009749e4c4b81070bb1b3cad84f6899f6a4
Graham Knapp <graham.knapp@acernis.fr>
2022-03-03 23:02:41 +0100

    First tests passing in Breton

--- num2words/__init__.py
+++ num2words/__init__.py
@@ -23,10 +23,11 @@ from . import (lang_AR, lang_CZ, lang_DE, lang_DK, lang_EN, lang_EN_IN,
                lang_IT, lang_JA, lang_KN, lang_KO, lang_KZ, lang_LT, lang_LV,
                lang_NL, lang_NO, lang_PL, lang_PT, lang_PT_BR, lang_RO,
                lang_RU, lang_SL, lang_SR, lang_SV, lang_TE, lang_TH, lang_TR,
-               lang_UK, lang_VI)
+               lang_UK, lang_VI, lang_BR)
 
 CONVERTER_CLASSES = {
     'ar': lang_AR.Num2Word_AR(),
+    'br': lang_BR.Num2Word_BR(),
     'cz': lang_CZ.Num2Word_CZ(),
     'en': lang_EN.Num2Word_EN(),
     'en_IN': lang_EN_IN.Num2Word_EN_IN(),
