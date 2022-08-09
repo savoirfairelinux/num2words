a7442708fabe210555356fb4c0ac9fc7ae9a92ff
Eyosiyas Bereketab <mreyosivip@gmail.com>
2022-08-09 21:19:56 +0300

    Update __init__.py

--- num2words/__init__.py
+++ num2words/__init__.py
@@ -23,7 +23,7 @@ from . import (lang_AM, lang_AR, lang_CZ, lang_DE, lang_DK, lang_EN,
                lang_HE, lang_HU, lang_ID, lang_IT, lang_JA, lang_KN, lang_KO,
                lang_KZ, lang_LT, lang_LV, lang_NL, lang_NO, lang_PL, lang_PT,
                lang_PT_BR, lang_RO, lang_RU, lang_SL, lang_SR, lang_SV,
-               lang_TE, lang_TH, lang_TR, lang_UK, lang_VI)
+               lang_TE, lang_TG, lang_TH, lang_TR, lang_UK, lang_VI)
 
 CONVERTER_CLASSES = {
     'am': lang_AM.Num2Word_AM(),
