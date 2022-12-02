36a025b498046720daf81caf171322af03b2cc4c
eyaler <eyalgruss@gmail.com>
2022-12-02 13:43:16 +0000

    change hebrew spelling from biblical and short-form (ktiv haser) to modern long-form (ktiv male); add hebrew gender; and hebrew ordinals; add hebrew decimal fraction and negative; fix hebrew word order for singular; fix hebrew currency names; fix global double space after minus; fix tests

--- num2words/lang_HE.py
+++ num2words/lang_HE.py
@@ -19,33 +19,36 @@
 from __future__ import print_function, unicode_literals
 
 from .base import Num2Word_Base
+from .compat import to_s
+from .currency import parse_currency_parts, prefix_currency
 from .utils import get_digits, splitbyx
+
 
 ZERO = (u'אפס',)
 
 ONES = {
-    1: (u'אחת',),
-    2: (u'שתיים',),
-    3: (u'שלוש',),
-    4: (u'ארבע',),
-    5: (u'חמש',),
-    6: (u'שש',),
-    7: (u'שבע',),
-    8: (u'שמונה',),
-    9: (u'תשע',),
+    1: (u'אחת', u'אחד', u'ראשונה', u'ראשון'),
+    2: (u'שתיים', u'שניים', u'שנייה', u'שני'),
+    3: (u'שלוש', u'שלושה', u'שלישית', u'שלישי'),
+    4: (u'ארבע', u'ארבעה', u'רביעית', u'רביעי'),
+    5: (u'חמש', u'חמישה', u'חמישית', u'חמישי'),
+    6: (u'שש', u'שישה', u'שישית', u'שישי'),
+    7: (u'שבע', u'שבעה', u'שביעית', u'שביעי'),
+    8: (u'שמונה', u'שמונה', u'שמינית', u'שמיני'),
+    9: (u'תשע', u'תשעה', u'תשיעית', u'תשיעי'),
 }
 
 TENS = {
-    0: (u'עשר',),
-    1: (u'אחת עשרה',),
-    2: (u'שתים עשרה',),
-    3: (u'שלוש עשרה',),
-    4: (u'ארבע עשרה',),
-    5: (u'חמש עשרה',),
-    6: (u'שש עשרה',),
-    7: (u'שבע עשרה',),
-    8: (u'שמונה עשרה',),
-    9: (u'תשע עשרה',),
+    0: (u'עשר', u'עשרה', u'עשירית', u'עשירי'),
+    1: (u'אחת עשרה', u'אחד עשר'),
+    2: (u'שתים עשרה', u'שנים עשר'),
+    3: (u'שלוש עשרה', u'שלושה עשר'),
+    4: (u'ארבע עשרה', u'ארבעה עשר'),
+    5: (u'חמש עשרה', u'חמישה עשר'),
+    6: (u'שש עשרה', u'שישה עשר'),
+    7: (u'שבע עשרה', u'שבעה עשר'),
+    8: (u'שמונה עשרה', u'שמונה עשר'),
+    9: (u'תשע עשרה', u'תשעה עשר'),
 }
 
 TWENTIES = {
@@ -79,14 +82,11 @@ THOUSANDS = {
 
 AND = u'ו'
 
+MAXVAL = 10000
 
-def pluralize(n, forms):
-    form = 1 if n == 0 else 0 if n == 1 else 1
-    return forms[form]
-
-
-def int2word(n):
-    if n > 9999:  # doesn't yet work for numbers this big
+def int2word(n, gender='f', ordinal=False):
+    assert n == int(n)
+    if n >= MAXVAL:  # doesn't yet work for numbers this big
         raise NotImplementedError()
 
     if n == 0:
@@ -118,47 +118,126 @@ def int2word(n):
             words.append(TWENTIES[n2][0])
 
         if n2 == 1:
-            words.append(TENS[n1][0])
+            words.append(TENS[n1][(gender == 'm') + ordinal*(not n1)])
         elif n1 > 0 and not (i > 0 and x == 1):
-            words.append(ONES[n1][0])
+            words.append(ONES[n1][(gender == 'm') + ordinal*(x < 11)])
 
         if i > 0:
             words.append(THOUSANDS[i][0])
 
-    # source: https://hebrew-academy.org.il/2017/01/30/ו-החיבור-במספרים/
+    # source: https://hebrew-academy.org.il/2017/01/30/%D7%95-%D7%94%D7%97%D7%99%D7%91%D7%95%D7%A8-%D7%91%D7%9E%D7%A1%D7%A4%D7%A8%D7%99%D7%9D
     if len(words) > 1:
         words[-1] = AND + words[-1]
 
     return ' '.join(words)
 
 
-def n2w(n):
-    return int2word(int(n))
-
-
 class Num2Word_HE(Num2Word_Base):
     CURRENCY_FORMS = {
-        'NIS': (('שקל', 'שקלים'), ('אגורה', 'אגורות')),
-        'EUR': (('אירו', 'אירו'), ('סנט', 'סנט')),
-        'USD': (('דולר', 'דולרים'), ('סנט', 'סנט')),
+        'ILS': ((u'שקל', u'שקלים'), (u'אגורה', u'אגורות')),
+        'EUR': ((u'אירו', u'אירו'), (u'סנט', u'סנטים')),
+        'USD': ((u'דולר', u'דולרים'), (u'סנט', u'סנטים')),
     }
 
-    def to_cardinal(self, number):
-        return n2w(number)
+    CURRENCY_GENDERS = {
+        'ILS': ('m', 'f'),
+        'EUR': ('m', 'm'),
+        'USD': ('m', 'm'),
+    }
 
-    def to_ordinal(self, number):
-        raise NotImplementedError()
+    def setup(self):
+        super(Num2Word_HE, self).setup()
+
+        self.negword = u"מינוס"
+        self.pointword = u"נקודה"
+        self.MAXVAL = MAXVAL
+
+    def to_cardinal_float(self, value, gender='f'):
+        try:
+            float(value) == value
+        except (ValueError, TypeError, AssertionError, AttributeError):
+            raise TypeError(self.errmsg_nonnum % value)
+
+        pre, post = self.float2tuple(float(value))
+
+        post = str(post)
+        post = '0' * (self.precision - len(post)) + post
+
+        out = [self.to_cardinal(pre, gender=gender)]
+        if self.precision:
+            out.append(self.title(self.pointword))
+
+        for i in range(self.precision):
+            curr = int(post[i])
+            out.append(to_s(self.to_cardinal(curr)))
+
+        return " ".join(out)
+
+
+    def to_cardinal(self, value, gender='f'):
+        try:
+            assert int(value) == value
+        except (ValueError, TypeError, AssertionError):
+            return self.to_cardinal_float(value, gender)  # https://hebrew-academy.org.il/2019/12/03/%D7%A2%D7%9C-%D7%94%D7%91%D7%A2%D7%AA-%D7%94%D7%9E%D7%A1%D7%A4%D7%A8-%D7%94%D7%9E%D7%A2%D7%95%D7%A8%D7%91/
+
+        out = ""
+        if value < 0:
+            value = abs(value)
+            out = "%s " % self.negword.strip()
+
+        if value >= self.MAXVAL:
+            raise OverflowError(self.errmsg_toobig % (value, self.MAXVAL))
+
+        return out + int2word(int(value), gender, ordinal=False)
+
+    def to_ordinal(self, value, gender='m'):
+        self.verify_ordinal(value)
+        out = int2word(int(value), gender, ordinal=True)
+        if value == 0 or value > 10:
+            out = 'ה' + out
+        return out
 
     def pluralize(self, n, forms):
-        return pluralize(n, forms)
+        assert n == int(n)
+        form = 0 if n == 1 else 1
+        return forms[form]
 
-    def to_currency(self, val, currency='NIS', cents=True, separator=' ו',
+
+    def to_currency(self, val, currency='ILS', cents=True, separator=' ' + AND,
                     adjective=False):
-        result = super(Num2Word_HE, self).to_currency(
-            val, currency=currency, cents=cents, separator=separator,
-            adjective=adjective)
-        # In Hebrew the separator is along with the following word
-        return result.replace(" ו ", " ו")
+        left, right, is_negative = parse_currency_parts(val)
+
+        try:
+            cr1, cr2 = self.CURRENCY_FORMS[currency]
+
+        except KeyError:
+            raise NotImplementedError(
+                'Currency code "%s" not implemented for "%s"' %
+                (currency, self.__class__.__name__))
+
+        minus_str = "%s " % self.negword.strip() if is_negative else ""
+        try:
+            gender1, gender2 = self.CURRENCY_GENDERS[currency]
+        except KeyError:
+            gender1 = gender2 = ''
+
+        money_str = self.to_cardinal(left, gender=gender1)
+        cents_str = self.to_cardinal(right, gender=gender2) \
+            if cents else self._cents_terse(right, currency)
+
+        strings = [
+            minus_str,
+            money_str,
+            self.pluralize(left, cr1),
+            separator,
+            cents_str,
+            self.pluralize(right, cr2)
+        ]
+        if left == 1:
+            strings[1], strings[2] = strings[2], strings[1]
+        if right == 1:
+            strings[4], strings[5] = strings[5], strings[4]
+        return u'%s%s %s%s%s %s' % tuple(strings) # In Hebrew the separator is along with the following word
 
 
 if __name__ == '__main__':
--- tests/test_he.py
+++ tests/test_he.py
@@ -31,25 +31,25 @@ class Num2WordsHETest(TestCase):
 
     def test_1_to_10(self):
         self.assertEqual(num2words(1, lang="he"), u'אחת')
-        self.assertEqual(num2words(2, lang="he"), u'שתים')
+        self.assertEqual(num2words(2, lang="he"), u'שתיים')
         self.assertEqual(num2words(7, lang="he"), u'שבע')
         self.assertEqual(num2words(10, lang="he"), u'עשר')
 
     def test_11_to_19(self):
         self.assertEqual(num2words(11, lang="he"), u'אחת עשרה')
-        self.assertEqual(num2words(13, lang="he"), u'שלש עשרה')
+        self.assertEqual(num2words(13, lang="he"), u'שלוש עשרה')
         self.assertEqual(num2words(15, lang="he"), u'חמש עשרה')
         self.assertEqual(num2words(16, lang="he"), u'שש עשרה')
         self.assertEqual(num2words(19, lang="he"), u'תשע עשרה')
 
     def test_20_to_99(self):
         self.assertEqual(num2words(20, lang="he"), u'עשרים')
-        self.assertEqual(num2words(23, lang="he"), u'עשרים ושלש')
+        self.assertEqual(num2words(23, lang="he"), u'עשרים ושלוש')
         self.assertEqual(num2words(28, lang="he"), u'עשרים ושמונה')
-        self.assertEqual(num2words(31, lang="he"), u'שלשים ואחת')
+        self.assertEqual(num2words(31, lang="he"), u'שלושים ואחת')
         self.assertEqual(num2words(40, lang="he"), u'ארבעים')
-        self.assertEqual(num2words(66, lang="he"), u'ששים ושש')
-        self.assertEqual(num2words(92, lang="he"), u'תשעים ושתים')
+        self.assertEqual(num2words(66, lang="he"), u'שישים ושש')
+        self.assertEqual(num2words(92, lang="he"), u'תשעים ושתיים')
 
     def test_100_to_999(self):
         self.assertEqual(num2words(100, lang="he"), u'מאה')
@@ -65,7 +65,7 @@ class Num2WordsHETest(TestCase):
         self.assertEqual(num2words(1001, lang="he"), u'אלף ואחת')
         self.assertEqual(num2words(1500, lang="he"), u'אלף וחמש מאות')
         self.assertEqual(
-            num2words(7378, lang="he"), u'שבעת אלפים שלש מאות שבעים ושמונה'
+            num2words(7378, lang="he"), u'שבעת אלפים שלוש מאות שבעים ושמונה'
         )
         self.assertEqual(num2words(2000, lang="he"), u'אלפיים')
         self.assertEqual(num2words(2100, lang="he"), u'אלפיים ומאה')
@@ -75,42 +75,67 @@ class Num2WordsHETest(TestCase):
 
     def test_pluralize(self):
         n = Num2Word_HE()
-        cr1, cr2 = n.CURRENCY_FORMS['NIS']
-        self.assertEqual(n.pluralize(1, cr1), 'שקל')
-        self.assertEqual(n.pluralize(2, cr1), 'שקלים')
-        self.assertEqual(n.pluralize(1, cr2), 'אגורה')
-        self.assertEqual(n.pluralize(2, cr2), 'אגורות')
+        cr1, cr2 = n.CURRENCY_FORMS['ILS']
+        self.assertEqual(n.pluralize(1, cr1), u'שקל')
+        self.assertEqual(n.pluralize(2, cr1), u'שקלים')
+        self.assertEqual(n.pluralize(1, cr2), u'אגורה')
+        self.assertEqual(n.pluralize(2, cr2), u'אגורות')
 
         cr1, cr2 = n.CURRENCY_FORMS['USD']
-        self.assertEqual(n.pluralize(1, cr1), 'דולר')
-        self.assertEqual(n.pluralize(2, cr1), 'דולרים')
-        self.assertEqual(n.pluralize(1, cr2), 'סנט')
-        self.assertEqual(n.pluralize(2, cr2), 'סנט')
+        self.assertEqual(n.pluralize(1, cr1), u'דולר')
+        self.assertEqual(n.pluralize(2, cr1), u'דולרים')
+        self.assertEqual(n.pluralize(1, cr2), u'סנט')
+        self.assertEqual(n.pluralize(2, cr2), u'סנטים')
 
     def test_to_currency(self):
         n = Num2Word_HE()
         self.assertEqual(
-            n.to_currency(20.0, currency='NIS'), 'עשרים שקלים ואפס אגורות'
+            n.to_currency(20.0, currency='ILS'), u'עשרים שקלים ואפס אגורות'
         )
         self.assertEqual(
-           n.to_currency(100.0, currency='NIS'), 'מאה שקלים ואפס אגורות'
+           n.to_currency(100.0, currency='ILS'), u'מאה שקלים ואפס אגורות'
         )
         self.assertEqual(
-           n.to_currency(100.50, currency='NIS'), 'מאה שקלים וחמישים אגורות'
+           n.to_currency(100.50, currency='ILS'), u'מאה שקלים וחמישים אגורות'
+        )
+        self.assertEqual(
+            n.to_currency(101.51, currency='ILS'), u'מאה ואחד שקלים וחמישים ואחת אגורות'
+        )
+        self.assertEqual(
+            n.to_currency(-101.51, currency='ILS'), u'מינוס מאה ואחד שקלים וחמישים ואחת אגורות'
+        )
+        self.assertEqual(
+            n.to_currency(1.01, currency='ILS'), u'שקל אחד ואגורה אחת'
+        )
+        self.assertEqual(
+            n.to_currency(-1.01, currency='ILS'), u'מינוס שקל אחד ואגורה אחת'
         )
 
     def test_to_cardinal(self):
         n = Num2Word_HE()
         self.assertEqual(n.to_cardinal(1500), u'אלף וחמש מאות')
-
-
-class Num2WordsHETestNotImplementedMethofs(TestCase):
-    n = Num2Word_HE()
+        self.assertEqual(n.to_cardinal(1501), u'אלף חמש מאות ואחת')
 
     def test_to_ordinal(self):
-        with self.assertRaises(NotImplementedError):
-            self.n.to_ordinal('1')
+        n = Num2Word_HE()
+        self.assertEqual(n.to_ordinal(1500), u'האלף וחמש מאות')
+        self.assertEqual(n.to_ordinal(1501), u'האלף חמש מאות ואחד')
+
+    def test_cardinal_for_float_number(self):
+        self.assertEqual(num2words(12.5, lang='he'), u'שתים עשרה נקודה חמש')
+        self.assertEqual(num2words(12.51, lang='he'), u'שתים עשרה נקודה חמש אחת')
+        self.assertEqual(num2words(12.53, lang='he'), u'שתים עשרה נקודה חמש שלוש')
+        self.assertEqual(num2words(12.59, lang='he'), u'שתים עשרה נקודה חמש תשע')
+        self.assertEqual(num2words(12.5, lang='he', gender='m'), u'שנים עשר נקודה חמש')
+        self.assertEqual(num2words(12.51, lang='he', gender='m'), u'שנים עשר נקודה חמש אחת')
+        self.assertEqual(num2words(12.53, lang='he', gender='m'), u'שנים עשר נקודה חמש שלוש')
+        self.assertEqual(num2words(12.59, lang='he', gender='m'), u'שנים עשר נקודה חמש תשע')
+        self.assertEqual(num2words(12.594132, lang='he', gender='m'), u'שנים עשר נקודה חמש תשע ארבע אחת שלוש שתיים')
+
+
+class Num2WordsHETestNotImplementedMethods(TestCase):
+    n = Num2Word_HE()
 
     def test_large_number(self):
-        with self.assertRaises(NotImplementedError):
+        with self.assertRaises(OverflowError):
             num2words(2000000, lang="he")
