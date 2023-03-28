00836e13e00fa5986526fdd7b841dc1ef44f15f8
Mario Monroy <m.monroyc22@gmail.com>
2023-03-28 10:57:57 -0600

    Guatemala currency

--- num2words/__init__.py
+++ num2words/__init__.py
@@ -18,13 +18,13 @@
 from __future__ import unicode_literals
 
 from . import (lang_AM, lang_AR, lang_AZ, lang_CZ, lang_DE, lang_DK, lang_EN,
-               lang_EN_IN, lang_EO, lang_ES, lang_ES_CO, lang_ES_NI,
-               lang_ES_VE, lang_FA, lang_FI, lang_FR, lang_FR_BE, lang_FR_CH,
-               lang_FR_DZ, lang_HE, lang_HU, lang_ID, lang_IS, lang_IT,
-               lang_JA, lang_KN, lang_KO, lang_KZ, lang_LT, lang_LV, lang_NL,
-               lang_NO, lang_PL, lang_PT, lang_PT_BR, lang_RO, lang_RU,
-               lang_SL, lang_SR, lang_SV, lang_TE, lang_TG, lang_TH, lang_TR,
-               lang_UK, lang_VI)
+               lang_EN_IN, lang_EO, lang_ES, lang_ES_CO, lang_ES_GT,
+               lang_ES_NI, lang_ES_VE, lang_FA, lang_FI, lang_FR, lang_FR_BE,
+               lang_FR_CH, lang_FR_DZ, lang_HE, lang_HU, lang_ID, lang_IS,
+               lang_IT, lang_JA, lang_KN, lang_KO, lang_KZ, lang_LT, lang_LV,
+               lang_NL, lang_NO, lang_PL, lang_PT, lang_PT_BR, lang_RO,
+               lang_RU, lang_SL, lang_SR, lang_SV, lang_TE, lang_TG, lang_TH,
+               lang_TR, lang_UK, lang_VI)
 
 CONVERTER_CLASSES = {
     'am': lang_AM.Num2Word_AM(),
@@ -45,6 +45,7 @@ CONVERTER_CLASSES = {
     'es_CO': lang_ES_CO.Num2Word_ES_CO(),
     'es_NI': lang_ES_NI.Num2Word_ES_NI(),
     'es_VE': lang_ES_VE.Num2Word_ES_VE(),
+    'es_GT': lang_ES_GT.Num2Word_ES_GT(),
     'id': lang_ID.Num2Word_ID(),
     'ja': lang_JA.Num2Word_JA(),
     'kn': lang_KN.Num2Word_KN(),
