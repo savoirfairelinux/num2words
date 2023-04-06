02a04c91ac206d1087b334b572043e8b262c1d14
Mario Monroy <m.monroyc22@gmail.com>
2023-04-05 18:54:58 -0600

    fixes flake8

--- num2words/lang_ES_GT.py
+++ num2words/lang_ES_GT.py
@@ -23,7 +23,10 @@ from .lang_ES import Num2Word_ES
 class Num2Word_ES_GT(Num2Word_ES):
 
     def to_currency(self, val, longval=True, old=False):
-        result = self.to_splitnum(val, hightxt="quetzal/es", lowtxt="centavo/s",
-                                  divisor=1, jointxt="y", longval=longval)
-        # Handle exception, in spanish is "un euro" and not "uno euro"
+        result = self.to_splitnum(val, hightxt="quetzal/es",
+                                  lowtxt="centavo/s",
+                                  divisor=1, jointxt="y",
+                                  longval=longval)
+        # Handle exception, in spanish is "un euro"
+        # and not "uno euro"
         return result.replace("uno", "un")
