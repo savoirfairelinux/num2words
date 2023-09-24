edb3aa1745345f541899f86a1c0dc51f7a6fc71f
Innocent Zenda <zendainnocent@gmail.com>
2023-09-24 17:24:35 +0300

    handle big numbers

--- num2words/lang_SW.py
+++ num2words/lang_SW.py
@@ -13,6 +13,19 @@ ONES = [
     'tisa',
 ]
 
+ONES_STR = {
+    0: 'sifuri',
+    1: 'moja',
+    2: 'mbili',
+    3: 'tatu',
+    4: 'nne',
+    5: 'tano',
+    6: 'sita',
+    7: 'saba',
+    8: 'nane',
+    9: 'tisa',
+}
+
 TENS_STR = {
     1: 'kumi',
     2: 'ishirini',
@@ -25,25 +38,38 @@ TENS_STR = {
     9: 'tisini',
 }
 
+EXPONENT_PREFIXES = {
+    6: 'm',
+    9: 'b',
+    12: 'tr',
+    15: 'quadr',
+    18: 'quint',
+    21: 'sext',
+    24: 'sept',
+    27: 'oct',
+    30: 'non',
+    33: 'dec',
+}
+
 
 class Num2Word_SW(Num2Word_Base):
     MINUS_PREFIX_WORD = 'hasi '
 
     def tens_to_cardinal(self, number):
+        '''Converts a number less than 100 to a string.'''
         tens = number // 10
-        units = number % 10
+        remainder = number % 10
 
-        if tens in TENS_STR:
-            prefix = TENS_STR[tens]
+        prefix = TENS_STR[tens]
+        postfix = ONES[remainder]
 
-        postfix = ONES[units]
-
-        if units == 0:
+        if remainder == 0:
             return prefix
 
         return f'{prefix} na {postfix}'
 
     def hundreds_to_cardinal(self, number):
+        '''Converts a number less than 1000 to a string.'''
         hundreds = number // 100
         remainder = number % 100
 
@@ -51,31 +77,106 @@ class Num2Word_SW(Num2Word_Base):
             return f'mia {ONES[hundreds]}'
         elif remainder < 10:
             return f'mia {ONES[hundreds]} na {ONES[remainder]}'
-        elif remainder < 100:
+        else:
             return f'mia {ONES[hundreds]} {self.tens_to_cardinal(remainder)}'
 
     def thousands_to_cardinal(self, number):
+        '''Converts a number less than 1_000_000 to a string.'''
         thousands = number // 1000
         remainder = number % 1000
 
         if remainder == 0:
-            return f'elfu {ONES[thousands]}'
+            if thousands < 10:
+                # thousands = 1 - 9
+                return f'elfu {ONES[thousands]}'
+            elif thousands < 100:
+                # thousands = 10 - 99
+                return f'{self.tens_to_cardinal(thousands)} elfu'
+            else:
+                # thousands = 100 - 999
+                return f'{self.hundreds_to_cardinal(thousands)} elfu'
         elif remainder < 10:
-            return f'{ONES[thousands]} elfu na {ONES[remainder]}'
+            if thousands < 10:
+                # thousands = 1 - 9
+                return f'{ONES[thousands]} elfu na {ONES[remainder]}'
+            elif thousands < 100:
+                # thousands = 10 - 99
+                return f'{self.tens_to_cardinal(thousands)} elfu na {ONES[remainder]}'
+            elif thousands < 1000:
+                # thousands = 100 - 999
+                return (
+                    f'{self.hundreds_to_cardinal(thousands)} elfu na {ONES[remainder]}'
+                )
         elif remainder < 100:
-            return f'{ONES[thousands]} elfu na {self.tens_to_cardinal(remainder)}'
+            if thousands < 10:
+                # thousands = 1 - 9
+                return f'{ONES[thousands]} elfu na {self.tens_to_cardinal(remainder)}'
+            elif thousands < 100:
+                # thousands = 10 - 99
+                return f'{self.tens_to_cardinal(thousands)} elfu na {self.tens_to_cardinal(remainder)}'
+            elif thousands < 1000:
+                # thousands = 100 - 999
+                return f'{self.hundreds_to_cardinal(thousands)} elfu, {self.tens_to_cardinal(remainder)}'
         elif remainder < 1000:
-            return f'{ONES[thousands]} elfu, {self.hundreds_to_cardinal(remainder)}'
+            if thousands < 10:
+                # thousands = 1 - 9
+                return f'{ONES[thousands]} elfu, {self.hundreds_to_cardinal(remainder)}'
+            elif thousands < 100:
+                # thousands = 10 - 99
+                return f'{self.tens_to_cardinal(thousands)} elfu, {self.hundreds_to_cardinal(remainder)}'
+            else:
+                # thousands = 100 - 999
+                return f'{self.hundreds_to_cardinal(thousands)} elfu, {self.hundreds_to_cardinal(remainder)}'
+
+    def big_number_to_cardinal(self, number):
+        digits = [c for c in str(number)]
+        length = len(digits)
+        if length >= 66:
+            raise NotImplementedError('The given number is too large.')
+
+        predigits = length % 3 or 3
+        num = int(''.join(digits[:predigits]))
+        if num < 10:
+            num_in_words = ONES_STR[num]
+        elif num < 100:
+            num_in_words = self.tens_to_cardinal(num)
+        else:
+            num_in_words = self.hundreds_to_cardinal(num)
+
+        word = EXPONENT_PREFIXES[len(digits[predigits:])] + 'ilioni'
+        base = f'{num_in_words} {word}'
+        number = int(''.join(digits[predigits:]))  # remaining number
+
+        if number == 0:
+            return f'{word} {num_in_words}'
+        elif number < 10:
+            return f'{base} na {ONES[number]}'
+        elif number < 100:
+            return f'{base} na {self.tens_to_cardinal(number)}'
+        elif number < 1000:
+            return f'{base}, {self.hundreds_to_cardinal(number)}'
+        elif number < 1_000_000:
+            return f'{base}, {self.thousands_to_cardinal(number)}'
+        else:
+            return f'{base}, {self.big_number_to_cardinal(number)}'
 
     def to_cardinal(self, number):
         if number < 0:
+            # negative number
             string = self.MINUS_PREFIX_WORD + self.to_cardinal(-number)
         elif number < 10:
-            string = ONES[number]
+            # 1 - 9
+            string = ONES[int(number)]
         elif number < 100:
-            string = self.tens_to_cardinal(number)
+            # 10 - 99
+            string = self.tens_to_cardinal(int(number))
         elif number < 1000:
+            # 100 - 999
             string = self.hundreds_to_cardinal(number)
-        elif number < 10_000:
-            string = self.thousands_to_cardinal(number)
+        elif number < 1_000_000:
+            # 100 - 999999
+            string = self.thousands_to_cardinal(int(number))
+        else:
+            # 1_000_000 - Infinity
+            string = self.big_number_to_cardinal(int(number))
         return string
--- tests/test_sw.py
+++ tests/test_sw.py
@@ -23,12 +23,21 @@ from num2words import num2words
 
 
 class Num2WordsSWTest(TestCase):
+    def test_negative(self):
+        self.assertEqual(num2words(-1, lang='sw'), 'hasi moja')
+
     def test_0(self):
         self.assertEqual(num2words(0, lang='sw'), 'sifuri')
 
     def test_1_to_9(self):
         self.assertEqual(num2words(1, lang='sw'), 'moja')
         self.assertEqual(num2words(2, lang='sw'), 'mbili')
+        self.assertEqual(num2words(3, lang='sw'), 'tatu')
+        self.assertEqual(num2words(4, lang='sw'), 'nne')
+        self.assertEqual(num2words(5, lang='sw'), 'tano')
+        self.assertEqual(num2words(6, lang='sw'), 'sita')
+        self.assertEqual(num2words(7, lang='sw'), 'saba')
+        self.assertEqual(num2words(8, lang='sw'), 'nane')
         self.assertEqual(num2words(9, lang='sw'), 'tisa')
 
     def test_tens_10_to_99(self):
@@ -47,10 +56,16 @@ class Num2WordsSWTest(TestCase):
         self.assertEqual(num2words(700, lang='sw'), 'mia saba')
         self.assertEqual(num2words(999, lang='sw'), 'mia tisa tisini na tisa')
 
-    def test_thousands_1000_to_9999(self):
+    def test_thousands_1000_to_999999(self):
         self.assertEqual(num2words(1000, lang='sw'), 'elfu moja')
+        self.assertEqual(num2words(1001, lang='sw'), 'moja elfu na moja')
         self.assertEqual(num2words(7000, lang='sw'), 'elfu saba')
         self.assertEqual(num2words(9000, lang='sw'), 'elfu tisa')
+        self.assertEqual(num2words(1008, lang='sw'), 'moja elfu na nane')
+        self.assertEqual(num2words(7012, lang='sw'), 'saba elfu na kumi na mbili')
+        self.assertEqual(
+            num2words(7312, lang='sw'), 'saba elfu, mia tatu kumi na mbili'
+        )
         self.assertEqual(
             num2words(1322, lang='sw'), 'moja elfu, mia tatu ishirini na mbili'
         )
@@ -59,4 +74,50 @@ class Num2WordsSWTest(TestCase):
         )
         self.assertEqual(
             num2words(9999, lang='sw'), 'tisa elfu, mia tisa tisini na tisa'
+        )
+        self.assertEqual(num2words(99000, lang='sw'), 'tisini na tisa elfu')
+        self.assertEqual(num2words(99001, lang='sw'), 'tisini na tisa elfu na moja')
+        self.assertEqual(num2words(70_000, lang='sw'), 'sabini elfu')
+        self.assertEqual(num2words(70_001, lang='sw'), 'sabini elfu na moja')
+        self.assertEqual(
+            num2words(99_999, lang='sw'),
+            'tisini na tisa elfu, mia tisa tisini na tisa',
+        )
+        self.assertEqual(num2words(100_001, lang='sw'), 'mia moja elfu na moja')
+        self.assertEqual(
+            num2words(999_999, lang='sw'),
+            'mia tisa tisini na tisa elfu, mia tisa tisini na tisa',
+        )
+
+    def test_big_numbers(self):
+        self.assertEqual(num2words(9_000_000, lang='sw'), 'milioni tisa')
+        self.assertEqual(num2words(9_000_001, lang='sw'), 'tisa milioni na moja')
+        self.assertEqual(num2words(9_000_007, lang='sw'), 'tisa milioni na saba')
+        self.assertEqual(num2words(9_000_010, lang='sw'), 'tisa milioni na kumi')
+        self.assertEqual(
+            num2words(9_000_317, lang='sw'), 'tisa milioni, mia tatu kumi na saba'
+        )
+        self.assertEqual(
+            num2words(4_001_235, lang='sw'),
+            'nne milioni, moja elfu, mia mbili thelathini na tano',
+        )
+        self.assertEqual(
+            num2words(3_081_369, lang='sw'),
+            'tatu milioni, themanini na moja elfu, mia tatu sitini na tisa',
+        )
+        self.assertEqual(
+            num2words(1_581_317, lang='sw'),
+            'moja milioni, mia tano themanini na moja elfu, mia tatu kumi na saba',
+        )
+        self.assertEqual(
+            num2words(911_581_317, lang='sw'),
+            'mia tisa kumi na moja milioni, mia tano themanini na moja elfu, mia tatu kumi na saba',
+        )
+        self.assertEqual(
+            num2words(1_911_581_317, lang='sw'),
+            'moja bilioni, mia tisa kumi na moja milioni, mia tano themanini na moja elfu, mia tatu kumi na saba',
+        )
+        self.assertEqual(
+            num2words(882_911_581_317, lang='sw'),
+            'mia nane themanini na mbili bilioni, mia tisa kumi na moja milioni, mia tano themanini na moja elfu, mia tatu kumi na saba',
         )
