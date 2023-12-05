7e6097aff961b2e1b8c4d1f5663f5fa228353ede
ajiragroup <108009061+ajiragroup@users.noreply.github.com>
2023-12-05 18:12:48 +0545

    Update lang_EN_NP.py
    edited class name

--- num2words/lang_EN_NP.py
+++ num2words/lang_EN_NP.py
@@ -23,7 +23,7 @@ from __future__ import unicode_literals
 from .lang_EN import Num2Word_EN
 
 
-class Num2Word_EN_IN(Num2Word_EN):
+class Num2Word_EN_NP(Num2Word_EN):
     def set_high_numwords(self, high):
         self.cards[10 ** 17] = "shankha"
         self.cards[10 ** 15] = "padam"
