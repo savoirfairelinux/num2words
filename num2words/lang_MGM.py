ed7524f656538290fae0eececc6b2e2b34ee3782
Mariano de Deus <dedeus_mariano@yahoo.com>
2024-03-28 10:18:00 +0900

    add mambae language

---
+++ num2words/lang_MGM.py
@@ -0,0 +1,223 @@
+# -*- coding: utf-8 -*-Num2Word_TET
+# Copyright (c) 2003, Taro Ogawa.  All Rights Reserved.
+# Copyright (c) 2013, Savoir-faire Linux inc.  All Rights Reserved.
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301 USA
+
+from __future__ import division, unicode_literals
+
+import re
+
+from .lang_EU import Num2Word_EU
+
+DOLLAR = ('dólar', 'dólares')
+CENTS = ('cêntimu', 'cêntimus')
+
+
+class Num2Word_MGM(Num2Word_EU):
+
+    CURRENCY_FORMS = {
+        'AUD': (DOLLAR, CENTS),
+        'CAD': (DOLLAR, CENTS),
+        'EUR': (('euru', 'eurus'), CENTS),
+        'GBP': (('libra', 'libras'), ('péni', 'pence')),
+        'USD': (DOLLAR, CENTS),
+    }
+
+    GIGA_SUFFIX = None
+    MEGA_SUFFIX = "iliaunid"
+
+    def setup(self):
+        super().setup()
+        lows = ["quatr", "tr", "b", "m"]
+        self.high_numwords = self.gen_high_numwords([], [], lows)
+        self.negword = "menus "
+        self.pointword = "vírgula"
+        self.exclude_title = ["resi", "vírgula", "menus"]
+
+        self.mid_numwords = [
+            (1000, "rihunid"), (100, "atusid"), (90, "gulhohopat"),
+            (80, "gulhohoteil"), (70, "gulhohoru"), (60, "gulhohonid"),
+            (50, "gullim"), (40, "gulpat"), (30, "gulteil"),
+            (20, "gulru")
+        ]
+        self.low_numwords = [
+            "sagul",
+            "hohopat", "hohoteil", "hohoru", "hohonid", "lim", "pat", "teil", "ru",
+            "id", "mamu"
+        ]
+        self.ords = [
+            {
+                0: "",
+                1: "primeiru",
+                2: "segundu",
+                3: "terceiru",
+                4: "quartu",
+                5: "quintu",
+                6: "sextu",
+                7: "sétimu",
+                8: "oitavu",
+                9: "nonu",
+            },
+            {
+                0: "",
+                1: "décimu",
+                2: "vigésimu",
+                3: "trigésimu",
+                4: "quadragésimu",
+                5: "quinquagésimu",
+                6: "sexagésimu",
+                7: "septuagésimu",
+                8: "octogésimu",
+                9: "nonagésimu",
+            },
+            {
+                0: "",
+                1: "centésimu",
+                2: "ducentésimu",
+                3: "tricentésimu",
+                4: "quadrigentésimu",
+                5: "quingentésimu",
+                6: "seiscentésimu",
+                7: "septigentésimu",
+                8: "octigentésimu",
+                9: "nongentésimu",
+            },
+        ]
+        self.thousand_separators = {
+            3: "milésimu",
+            6: "milionésimu",
+            9: "milésimu milionésimu",
+            12: "bilionésimu",
+            15: "milésimu bilionésimu"
+        }
+        self.hundreds = {
+            1: "atusid",
+            2: "atusid ru",
+            3: "atusid teil",
+            4: "atusid pat",
+            5: "atusid lim",
+            6: "atusid hohonid",
+            7: "atusid hohoru",
+            8: "atusid hohoteil",
+            9: "atusid hohopat",
+        }
+
+    def merge(self, curr, next):
+        ctext, cnum, ntext, nnum = curr + next
+
+        if cnum == 1 and nnum < 100:
+            return next
+
+        if nnum < cnum:
+            if nnum < 10:
+                return ("%s resi %s" % (ctext, ntext), cnum + nnum)
+            else:
+                return ("%s %s" % (ctext, ntext), cnum + nnum)
+
+        return (ntext + " " + ctext, cnum * nnum)
+
+    def to_cardinal(self, value):
+        result = super().to_cardinal(value)
+
+        # Transforms "mil e cento e catorze" into "mil cento e catorze"
+        # Transforms "cem milhões e duzentos mil e duzentos e dez" em "cem
+        # milhões duzentos mil duzentos e dez" but "cem milhões e duzentos
+        # mil e duzentos" in "cem milhões duzentos mil e duzentos" and not in
+        # "cem milhões duzentos mil duzentos"
+        for ext in (
+                'rihunid', 'miliaunid','miliaunid rihunid',
+                'biliaunid', 'biliaunid rihunid'):
+            if re.match('.*{} resi \\w*entus? (?=.*resi)'.format(ext), result):
+                result = result.replace(
+                    f'{ext} resi', f'{ext}'
+                )
+
+        return result
+
+    # for the ordinal conversion the code is similar to pt_BR code,
+    # although there are other rules that are probably more correct in
+    # Portugal. Concerning numbers from 2000th on, saying "dois
+    # milésimos" instead of "segundo milésimo" (the first number
+    # would be used in the cardinal form instead of the ordinal) is better.
+    # This was not implemented.
+    # source:
+    # https://ciberduvidas.iscte-iul.pt/consultorio/perguntas/a-forma-por-extenso-de-2000-e-de-outros-ordinais/16428
+    def to_ordinal(self, value):
+        # Before changing this function remember this is used by pt-BR
+        # so act accordingly
+        self.verify_ordinal(value)
+
+        result = []
+        value = str(value)
+        thousand_separator = ''
+
+        for idx, char in enumerate(value[::-1]):
+            if idx and idx % 3 == 0:
+                thousand_separator = self.thousand_separators[idx]
+
+            if char != '0' and thousand_separator:
+                # avoiding "segundo milionésimo milésimo" for 6000000,
+                # for instance
+                result.append(thousand_separator)
+                thousand_separator = ''
+
+            result.append(self.ords[idx % 3][int(char)])
+
+        result = ' '.join(result[::-1])
+        result = result.strip()
+        result = re.sub('\\s+', ' ', result)
+
+        if result.startswith('primeiru') and value != '1':
+            # avoiding "primeiro milésimo", "primeiro milionésimo" and so on
+            result = result[9:]
+
+        return result
+
+    def to_ordinal_num(self, value):
+        # Before changing this function remember this is used by pt-BR
+        # so act accordingly
+        self.verify_ordinal(value)
+        return "%sº" % (value)
+
+    def to_year(self, val, longval=True):
+        # Before changing this function remember this is used by pt-BR
+        # so act accordingly
+        if val < 0:
+            return self.to_cardinal(abs(val)) + ' antes Kristu'
+        return self.to_cardinal(val)
+
+    def to_currency(self, val, currency='EUR', cents=True, separator=' resi',
+                    adjective=False):
+        # change negword because base.to_currency() does not need space after
+        backup_negword = self.negword
+        self.negword = self.negword[:-1]
+        result = super().to_currency(
+            val, currency=currency, cents=cents, separator=separator,
+            adjective=adjective)
+        # undo the change on negword
+        self.negword = backup_negword
+
+        # transforms "milhões euros" em "milhões de euros"
+        cr1, _ = self.CURRENCY_FORMS[currency]
+
+        for ext in (
+                'miliaunid','biliaunid','triliaunid'):
+            if re.match('.*{} (?={})'.format(ext, cr1[1]), result):
+                result = result.replace(
+                    f'{ext}', f'{ext}', 1
+                )
+        # do not print "e zero cêntimos"
+        result = result.replace(' resi mamu cêntimus', '')
+        return result
---
+++ tests/test_mgm.py
@@ -0,0 +1,450 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2003, Taro Ogawa.  All Rights Reserved.
+# Copyright (c) 2013, Savoir-faire Linux inc.  All Rights Reserved.
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301 USA
+
+from __future__ import unicode_literals
+
+from decimal import Decimal
+from unittest import TestCase, skip
+
+from num2words import num2words
+from num2words.lang_TET import Num2Word_TET
+
+
+class Num2WordsTETTest(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.n2w = Num2Word_TET()
+
+    def test_cardinal_integer(self):
+        self.assertEqual(num2words(1, lang='tet'), 'ida')
+        self.assertEqual(num2words(2, lang='tet'), 'rua')
+        self.assertEqual(num2words(3, lang='tet'), 'tolu')
+        self.assertEqual(num2words(4, lang='tet'), 'haat')
+        self.assertEqual(num2words(5, lang='tet'), 'lima')
+        self.assertEqual(num2words(6, lang='tet'), 'neen')
+        self.assertEqual(num2words(7, lang='tet'), 'hitu')
+        self.assertEqual(num2words(8, lang='tet'), 'walu')
+        self.assertEqual(num2words(9, lang='tet'), 'sia')
+        self.assertEqual(num2words(10, lang='tet'), 'sanulu')
+        self.assertEqual(num2words(11, lang='tet'), 'sanulu resin ida')
+        self.assertEqual(num2words(12, lang='tet'), 'sanulu resin rua')
+        self.assertEqual(num2words(13, lang='tet'), 'sanulu resin tolu')
+        self.assertEqual(num2words(14, lang='tet'), 'sanulu resin haat')
+        self.assertEqual(num2words(15, lang='tet'), 'sanulu resin lima')
+        self.assertEqual(num2words(16, lang='tet'), 'sanulu resin neen')
+        self.assertEqual(num2words(17, lang='tet'), 'sanulu resin hitu')
+        self.assertEqual(num2words(18, lang='tet'), 'sanulu resin walu')
+        self.assertEqual(num2words(19, lang='tet'), 'sanulu resin sia')
+        self.assertEqual(num2words(20, lang='tet'), 'ruanulu')
+
+        self.assertEqual(num2words(21, lang='tet'), 'ruanulu resin ida')
+        self.assertEqual(num2words(22, lang='tet'), 'ruanulu resin rua')
+        self.assertEqual(num2words(35, lang='tet'), 'tolunulu resin lima')
+        self.assertEqual(num2words(99, lang='tet'), 'sianulu resin sia')
+
+        self.assertEqual(num2words(100, lang='tet'), 'atus ida')
+        self.assertEqual(num2words(101, lang='tet'), 'atus ida resin ida')
+        self.assertEqual(num2words(107, lang='tet'), 'atus ida resin hitu')
+        self.assertEqual(num2words(110, lang='tet'), 'atus ida sanulu')
+        self.assertEqual(num2words(114, lang='tet'), 'atus ida sanulu resin haat')
+        self.assertEqual(num2words(128, lang='tet'), 'atus ida ruanulu resin walu')
+        self.assertEqual(num2words(151, lang='tet'), 'atus ida limanulu resin ida')
+        self.assertEqual(num2words(713, lang='tet'), 'atus hitu sanulu resin tolu')
+        self.assertEqual(num2words(999, lang='tet'), 'atus sia sianulu resin sia')
+
+        self.assertEqual(num2words(1000, lang='tet'), 'rihun ida')
+        self.assertEqual(num2words(1001, lang='tet'), 'rihun ida resin ida')
+        self.assertEqual(num2words(1011, lang='tet'), 'rihun ida sanulu resin ida')
+        self.assertEqual(num2words(1111, lang='tet'), 'rihun ida atus ida sanulu resin ida')
+        self.assertEqual(num2words(2357, lang='tet'), 'rihun rua atus tolu limanulu resin hitu')
+        self.assertEqual(
+            num2words(2200, lang='tet'),
+            'rihun rua atus rua'
+        )
+        self.assertEqual(num2words(2230, lang='tet'), 'rihun rua atus rua tolunulu')
+        self.assertEqual(num2words(73400, lang='tet'), 'rihun hitunulu resin tolu atus haat')
+        self.assertEqual(num2words(73421, lang='tet'), 'rihun hitunulu resin tolu atus haat ruanulu resin ida')
+        self.assertEqual(num2words(100000, lang='tet'), 'rihun atus ida')
+        self.assertEqual(num2words(250050, lang='tet'), 'rihun atus rua limanulu limanulu')
+        self.assertEqual(
+            num2words(6000000, lang='tet'), 'miliaun neen'
+        )
+        self.assertEqual(
+            num2words(100000000, lang='tet'), 'miliaun atus ida'
+        )
+        self.assertEqual(
+            num2words(19000000000, lang='tet'), 'miliaun rihun sanulu resin sia'
+        )
+        self.assertEqual(
+            num2words(145000000002, lang='tet'),
+            'miliaun rihun atus ida haatnulu resin lima resin rua'
+        )
+        self.assertEqual(
+            num2words(4635102, lang='tet'),
+            'miliaun haat rihun atus neen tolunulu resin lima atus ida resin rua'
+        )
+        self.assertEqual(
+            num2words(145254635102, lang='tet'),
+            'miliaun rihun atus ida haatnulu resin lima atus rua limanulu resin haat rihun atus neen tolunulu resin lima atus ida resin rua'
+        )
+        self.assertEqual(
+            num2words(1000000000000, lang='tet'),
+            'biliaun ida'
+        )
+        self.assertEqual(
+            num2words(2000000000000, lang='tet'),
+            'biliaun rua'
+        )
+        self.assertEqual(
+            num2words(1000000000000000, lang='tet'),
+            'biliaun rihun ida'
+        )
+        self.assertEqual(
+            num2words(2000000000000000, lang='tet'),
+            'biliaun rihun rua'
+        )
+        self.assertEqual(
+            num2words(1000000000000000000, lang='tet'),
+            'triliaun ida'
+        )
+        self.assertEqual(
+            num2words(2000000000000000000, lang='tet'),
+            'triliaun rua'
+        )
+
+    def test_cardinal_integer_negative(self):
+        self.assertEqual(num2words(-1, lang='tet'), 'menus ida')
+        self.assertEqual(
+            num2words(-256, lang='tet'), 'menus atus rua limanulu resin neen'
+        )
+        self.assertEqual(num2words(-1000, lang='tet'), 'menus rihun ida')
+        self.assertEqual(num2words(-1000000, lang='tet'), 'menus miliaun ida')
+        self.assertEqual(
+            num2words(-1234567, lang='tet'),
+            'menus miliaun ida rihun atus rua tolunulu resin haat atus lima neenulu resin hitu'
+        )
+
+    def test_cardinal_float(self):
+        self.assertEqual(num2words(Decimal('1.00'), lang='tet'), 'ida')
+        self.assertEqual(num2words(
+            Decimal('1.01'), lang='tet'), 'ida vírgula zero ida')
+        self.assertEqual(num2words(
+            Decimal('1.035'), lang='tet'), 'ida vírgula zero tolu lima'
+        )
+        self.assertEqual(num2words(
+            Decimal('1.35'), lang='tet'), 'ida vírgula tolu lima'
+        )
+        self.assertEqual(
+            num2words(Decimal('3.14159'), lang='tet'),
+            'tolu vírgula ida haat ida lima sia'
+        )
+        self.assertEqual(
+            num2words(Decimal('101.22'), lang='tet'),
+            'atus ida resin ida vírgula rua rua'
+        )
+        self.assertEqual(
+            num2words(Decimal('2345.75'), lang='tet'),
+            'rihun rua atus tolu haatnulu resin lima vírgula hitu lima'
+        )
+
+
+    def test_cardinal_float_negative(self):
+        self.assertEqual(
+            num2words(Decimal('-2.34'), lang='tet'),
+            'menus rua vírgula tolu haat'
+        )
+        self.assertEqual(
+            num2words(Decimal('-9.99'), lang='tet'),
+            'menus sia vírgula sia sia'
+        )
+        self.assertEqual(
+            num2words(Decimal('-7.01'), lang='tet'),
+            'menus hitu vírgula zero ida'
+        )
+        self.assertEqual(
+            num2words(Decimal('-222.22'), lang='tet'),
+            'menus atus rua ruanulu resin rua vírgula rua rua'
+        )
+
+    def test_ordinal(self):
+        self.assertEqual(num2words(1, lang='tet', ordinal=True), 'primeiru')
+        self.assertEqual(num2words(2, lang='tet', ordinal=True), 'segundu')
+        self.assertEqual(num2words(3, lang='tet', ordinal=True), 'terceiru')
+        self.assertEqual(num2words(4, lang='tet', ordinal=True), 'quartu')
+        self.assertEqual(num2words(5, lang='tet', ordinal=True), 'quintu')
+        self.assertEqual(num2words(6, lang='tet', ordinal=True), 'sextu')
+        self.assertEqual(num2words(7, lang='tet', ordinal=True), 'sétimu')
+        self.assertEqual(num2words(8, lang='tet', ordinal=True), 'oitavu')
+        self.assertEqual(num2words(9, lang='tet', ordinal=True), 'nonu')
+        self.assertEqual(num2words(10, lang='tet', ordinal=True), 'décimu')
+        self.assertEqual(
+            num2words(11, lang='tet', ordinal=True), 'décimu primeiru'
+        )
+        self.assertEqual(
+            num2words(12, lang='tet', ordinal=True), 'décimu segundu'
+        )
+        self.assertEqual(
+            num2words(13, lang='tet', ordinal=True), 'décimu terceiru'
+        )
+        self.assertEqual(
+            num2words(14, lang='tet', ordinal=True), 'décimu quartu'
+        )
+        self.assertEqual(
+            num2words(15, lang='tet', ordinal=True), 'décimu quintu'
+        )
+        self.assertEqual(
+            num2words(16, lang='tet', ordinal=True), 'décimu sextu'
+        )
+        self.assertEqual(
+            num2words(17, lang='tet', ordinal=True), 'décimu sétimu'
+        )
+        self.assertEqual(
+            num2words(18, lang='tet', ordinal=True), 'décimu oitavu'
+        )
+        self.assertEqual(
+            num2words(19, lang='tet', ordinal=True), 'décimu nonu'
+        )
+        self.assertEqual(
+            num2words(20, lang='tet', ordinal=True), 'vigésimu'
+        )
+
+        self.assertEqual(
+            num2words(21, lang='tet', ordinal=True), 'vigésimu primeiru'
+        )
+        self.assertEqual(
+            num2words(22, lang='tet', ordinal=True), 'vigésimu segundu'
+        )
+        self.assertEqual(
+            num2words(35, lang='tet', ordinal=True), 'trigésimu quintu'
+        )
+        self.assertEqual(
+            num2words(99, lang='tet', ordinal=True), 'nonagésimu nonu'
+        )
+
+        self.assertEqual(
+            num2words(100, lang='tet', ordinal=True), 'centésimu'
+        )
+        self.assertEqual(
+            num2words(101, lang='tet', ordinal=True), 'centésimu primeiru'
+        )
+        self.assertEqual(
+            num2words(128, lang='tet', ordinal=True),
+            'centésimu vigésimu oitavu'
+        )
+        self.assertEqual(
+            num2words(713, lang='tet', ordinal=True),
+            'septigentésimu décimu terceiru'
+        )
+
+        self.assertEqual(
+            num2words(1000, lang='tet', ordinal=True), 'milésimu'
+        )
+        self.assertEqual(
+            num2words(1001, lang='tet', ordinal=True), 'milésimu primeiru'
+        )
+        self.assertEqual(
+            num2words(1111, lang='tet', ordinal=True),
+            'milésimu centésimu décimu primeiru'
+        )
+        self.assertEqual(
+            num2words(2114, lang='tet', ordinal=True),
+            'segundu milésimu centésimu décimu quartu'
+        )
+        self.assertEqual(
+            num2words(73421, lang='tet', ordinal=True),
+            'septuagésimu terceiru milésimu quadrigentésimu vigésimu primeiru'
+        )
+
+        self.assertEqual(
+            num2words(100000, lang='tet', ordinal=True),
+            'centésimu milésimu'
+        )
+        self.assertEqual(
+            num2words(250050, lang='tet', ordinal=True),
+            'ducentésimu quinquagésimu milésimu quinquagésimu'
+        )
+        self.assertEqual(
+            num2words(6000000, lang='tet', ordinal=True), 'sextu milionésimu'
+        )
+        self.assertEqual(
+            num2words(19000000000, lang='tet', ordinal=True),
+            'décimu nonu milésimu milionésimu'
+        )
+        self.assertEqual(
+            num2words(145000000002, lang='tet', ordinal=True),
+            'centésimu quadragésimu quintu milésimu milionésimu segundu'
+        )
+
+    def test_currency_integer(self):
+        self.assertEqual(self.n2w.to_currency(1.00), 'ida euru')
+        self.assertEqual(self.n2w.to_currency(2.00), 'rua eurus')
+        self.assertEqual(self.n2w.to_currency(3.00), 'tolu eurus')
+        self.assertEqual(self.n2w.to_currency(4.00), 'haat eurus')
+        self.assertEqual(self.n2w.to_currency(5.00), 'lima eurus')
+        self.assertEqual(self.n2w.to_currency(6.00), 'neen eurus')
+        self.assertEqual(self.n2w.to_currency(7.00), 'hitu eurus')
+        self.assertEqual(self.n2w.to_currency(8.00), 'walu eurus')
+        self.assertEqual(self.n2w.to_currency(9.00), 'sia eurus')
+        self.assertEqual(self.n2w.to_currency(10.00), 'sanulu eurus')
+        self.assertEqual(self.n2w.to_currency(11.00), 'sanulu resin ida eurus')
+        self.assertEqual(self.n2w.to_currency(12.00), 'sanulu resin rua eurus')
+        self.assertEqual(self.n2w.to_currency(13.00), 'sanulu resin tolu eurus')
+        self.assertEqual(self.n2w.to_currency(14.00), 'sanulu resin haat eurus')
+        self.assertEqual(self.n2w.to_currency(15.00), 'sanulu resin lima eurus')
+        self.assertEqual(self.n2w.to_currency(16.00), 'sanulu resin neen eurus')
+        self.assertEqual(self.n2w.to_currency(17.00), 'sanulu resin hitu eurus')
+        self.assertEqual(self.n2w.to_currency(18.00), 'sanulu resin walu eurus')
+        self.assertEqual(self.n2w.to_currency(19.00), 'sanulu resin sia eurus')
+        self.assertEqual(self.n2w.to_currency(20.00), 'ruanulu eurus')
+
+        self.assertEqual(self.n2w.to_currency(21.00), 'ruanulu resin ida eurus')
+        self.assertEqual(self.n2w.to_currency(22.00), 'ruanulu resin rua eurus')
+        self.assertEqual(self.n2w.to_currency(35.00), 'tolunulu resin lima eurus')
+        self.assertEqual(self.n2w.to_currency(99.00), 'sianulu resin sia eurus')
+
+        self.assertEqual(self.n2w.to_currency(100.00), 'atus ida eurus')
+        self.assertEqual(self.n2w.to_currency(101.00), 'atus ida resin ida eurus')
+        self.assertEqual(
+            self.n2w.to_currency(128.00), 'atus ida ruanulu resin walu eurus'
+        )
+        self.assertEqual(
+            self.n2w.to_currency(713.00), 'atus hitu sanulu resin tolu eurus')
+
+        self.assertEqual(self.n2w.to_currency(1000.00), 'rihun ida eurus')
+        self.assertEqual(self.n2w.to_currency(1001.00), 'rihun ida resin ida eurus')
+        self.assertEqual(
+            self.n2w.to_currency(1111.00), 'rihun ida atus ida sanulu resin ida eurus')
+        self.assertEqual(
+            self.n2w.to_currency(2114.00), 'rihun rua atus ida sanulu resin haat eurus'
+        )
+        self.assertEqual(
+            self.n2w.to_currency(73421.00),
+            'rihun hitunulu resin tolu atus haat ruanulu resin ida eurus'
+        )
+
+        self.assertEqual(self.n2w.to_currency(100000.00), 'rihun atus ida eurus')
+        self.assertEqual(
+            self.n2w.to_currency(250050.00),
+            'rihun atus rua limanulu limanulu eurus'
+        )
+        self.assertEqual(
+            self.n2w.to_currency(6000000.00),
+            'miliaun neen eurus'
+        )
+        self.assertEqual(
+            self.n2w.to_currency(19000000000.00),
+            'miliaun rihun sanulu resin sia eurus'
+        )
+        self.assertEqual(
+            self.n2w.to_currency(145000000002.00),
+            'miliaun rihun atus ida haatnulu resin lima resin rua eurus'
+        )
+        self.assertEqual(self.n2w.to_currency(1.00, currency='USD'),
+                         'ida dólar')
+        self.assertEqual(self.n2w.to_currency(1.50, currency='USD'),
+                         'ida dólar resin limanulu cêntimus')
+        with self.assertRaises(NotImplementedError):
+            self.n2w.to_currency(1.00, currency='CHF')
+
+    def test_currency_integer_negative(self):
+        self.assertEqual(self.n2w.to_currency(-1.00), 'menus ida euru')
+        self.assertEqual(
+            self.n2w.to_currency(-256.00),
+            'menus atus rua limanulu resin neen eurus'
+        )
+        self.assertEqual(self.n2w.to_currency(-1000.00), 'menus rihun ida eurus')
+        self.assertEqual(
+            self.n2w.to_currency(-1000000.00), 'menus miliaun ida eurus'
+        )
+        self.assertEqual(
+            self.n2w.to_currency(-1234567.00),
+            'menus miliaun ida rihun atus rua tolunulu resin haat atus lima neenulu resin hitu eurus'
+        )
+
+    def test_currency_float(self):
+        self.assertEqual(self.n2w.to_currency(Decimal('1.00')), 'ida euru')
+        self.assertEqual(
+            self.n2w.to_currency(Decimal('1.01')), 'ida euru resin ida cêntimu'
+        )
+        self.assertEqual(
+            self.n2w.to_currency(Decimal('1.03')), 'ida euru resin tolu cêntimus'
+        )
+        self.assertEqual(
+            self.n2w.to_currency(Decimal('1.35')),
+            'ida euru resin tolunulu resin lima cêntimus'
+        )
+        self.assertEqual(
+            self.n2w.to_currency(Decimal('3.14')),
+            'tolu eurus resin sanulu resin haat cêntimus'
+        )
+        self.assertEqual(
+            self.n2w.to_currency(Decimal('101.22')),
+            'atus ida resin ida eurus resin ruanulu resin rua cêntimus'
+        )
+        self.assertEqual(
+            self.n2w.to_currency(Decimal('2345.75')),
+            'rihun rua atus tolu haatnulu resin lima eurus resin hitunulu resin lima cêntimus'
+
+        )
+
+    def test_currency_float_negative(self):
+        self.assertEqual(
+            self.n2w.to_currency(Decimal('-2.34')),
+            'menus rua eurus resin tolunulu resin haat cêntimus'
+        )
+        self.assertEqual(
+            self.n2w.to_currency(Decimal('-9.99')),
+            'menus sia eurus resin sianulu resin sia cêntimus'
+        )
+        self.assertEqual(
+            self.n2w.to_currency(Decimal('-7.01')),
+            'menus hitu eurus resin ida cêntimu'
+        )
+        self.assertEqual(
+            self.n2w.to_currency(Decimal('-222.22')),
+            'menus atus rua ruanulu resin rua eurus resin ruanulu resin rua cêntimus'
+        )
+
+    def test_year(self):
+        self.assertEqual(self.n2w.to_year(1001), 'rihun ida resin ida')
+        self.assertEqual(
+            self.n2w.to_year(1789), 'rihun ida atus hitu walunulu resin sia'
+        )
+        self.assertEqual(
+            self.n2w.to_year(1942), 'rihun ida atus sia haatnulu resin rua'
+        )
+        self.assertEqual(
+            self.n2w.to_year(1984), 'rihun ida atus sia walunulu resin haat'
+        )
+        self.assertEqual(self.n2w.to_year(2000), 'rihun rua')
+        self.assertEqual(self.n2w.to_year(2001), 'rihun rua resin ida')
+        self.assertEqual(self.n2w.to_year(2016), 'rihun rua sanulu resin neen')
+
+    def test_year_negative(self):
+        self.assertEqual(self.n2w.to_year(-30), 'tolunulu antes Kristu')
+        self.assertEqual(
+            self.n2w.to_year(-744),
+            'atus hitu haatnulu resin haat antes Kristu'
+        )
+        self.assertEqual(
+            self.n2w.to_year(-10000),
+            'rihun sanulu antes Kristu'
+        )
+
+    def test_to_ordinal_num(self):
+        self.assertEqual(self.n2w.to_ordinal_num(1), '1º')
+        self.assertEqual(self.n2w.to_ordinal_num(100), '100º')
