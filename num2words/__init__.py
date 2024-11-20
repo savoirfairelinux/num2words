0df2a0ebc91e9ffba7c978f69e418a3f7271eba2
Marek Madejski <marekmadejski@yandex.com>
2024-11-20 17:36:21 +0100

    Czech language ISO 639-1 code fix (#587)

--- num2words/__init__.py
+++ num2words/__init__.py
@@ -18,7 +18,7 @@
 from __future__ import unicode_literals
 
 from . import (lang_AM, lang_AR, lang_AZ, lang_BE, lang_BN, lang_CA, lang_CE,
-               lang_CY, lang_CZ, lang_DE, lang_DK, lang_EN, lang_EN_IN,
+               lang_CS, lang_CY, lang_DE, lang_DK, lang_EN, lang_EN_IN,
                lang_EN_NG, lang_EO, lang_ES, lang_ES_CO, lang_ES_CR,
                lang_ES_GT, lang_ES_NI, lang_ES_VE, lang_FA, lang_FI, lang_FR,
                lang_FR_BE, lang_FR_CH, lang_FR_DZ, lang_HE, lang_HU, lang_ID,
@@ -35,8 +35,8 @@ CONVERTER_CLASSES = {
     'bn': lang_BN.Num2Word_BN(),
     'ca': lang_CA.Num2Word_CA(),
     'ce': lang_CE.Num2Word_CE(),
+    'cs': lang_CS.Num2Word_CS(),
     'cy': lang_CY.Num2Word_CY(),
-    'cz': lang_CZ.Num2Word_CZ(),
     'en': lang_EN.Num2Word_EN(),
     'en_IN': lang_EN_IN.Num2Word_EN_IN(),
     'en_NG': lang_EN_NG.Num2Word_EN_NG(),
