ad10ee6cfc37c40a8506963c9c83c3fd456aa71a
Max Palehin <59222687+xdownedx@users.noreply.github.com>
2025-05-12 14:27:38 +0300

    Add Armenian (HY) language support
    This commit adds full support for the Armenian language to the num2words library with the following features: Complete implementation of cardinal numbers in Armenian; Support for ordinal numbers with appropriate suffixes; Support for years with special cases handling; Currency support for multiple currencies including AMD, EUR, USD and others; Proper pluralization rules for Armenian nouns; Modern numeral representation using Arabic digits; Comprehensive test suite to ensure correctness. The implementation follows Armenian grammar rules and provides correct pronunciation for all numeric forms.

---
+++ num2words/lang_HY.py
@@ -0,0 +1,325 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2003, Taro Ogawa.  All Rights Reserved.
+# Copyright (c) 2013, Savoir-faire Linux inc.  All Rights Reserved.
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301 USA
+
+from __future__ import unicode_literals
+
+from .base import Num2Word_Base
+
+# Armenian numerals
+ONES = {
+    0: 'զրո',
+    1: 'մեկ',
+    2: 'երկու',
+    3: 'երեք',
+    4: 'չորս',
+    5: 'հինգ',
+    6: 'վեց',
+    7: 'յոթ',
+    8: 'ութ',
+    9: 'ինը'
+}
+
+TEENS = {
+    10: 'տասը',
+    11: 'տասնմեկ',
+    12: 'տասներկու',
+    13: 'տասներեք',
+    14: 'տասնչորս',
+    15: 'տասնհինգ',
+    16: 'տասնվեց',
+    17: 'տասնյոթ',
+    18: 'տասնութ',
+    19: 'տասնինը'
+}
+
+TENS = {
+    2: 'քսան',
+    3: 'երեսուն',
+    4: 'քառասուն',
+    5: 'հիսուն',
+    6: 'վաթսուն',
+    7: 'յոթանասուն',
+    8: 'ութսուն',
+    9: 'իննսուն'
+}
+
+HUNDREDS = {
+    1: 'հարյուր',
+    2: 'երկու հարյուր',
+    3: 'երեք հարյուր',
+    4: 'չորս հարյուր',
+    5: 'հինգ հարյուր',
+    6: 'վեց հարյուր',
+    7: 'յոթ հարյուր',
+    8: 'ութ հարյուր',
+    9: 'ինը հարյուր'
+}
+
+THOUSANDS = {
+    1: ('հազար', 'հազար'),
+    2: ('միլիոն', 'միլիոն'),
+    3: ('միլիարդ', 'միլիարդ'),
+    4: ('տրիլիոն', 'տրիլիոն'),
+    5: ('կվադրիլիոն', 'կվադրիլիոն'),
+    6: ('քվինտիլիոն', 'քվինտիլիոն'),
+    7: ('սեքստիլիոն', 'սեքստիլիոն'),
+    8: ('սեպտիլիոն', 'սեպտիլիոն'),
+    9: ('օկտիլիոն', 'օկտիլիոն'),
+    10: ('նոնիլիոն', 'նոնիլիոն'),
+}
+
+# Ordinal numerals
+ORDINAL_ONES = {
+    1: 'առաջին',
+    2: 'երկրորդ',
+    3: 'երրորդ',
+    4: 'չորրորդ',
+    5: 'հինգերորդ',
+    6: 'վեցերորդ',
+    7: 'յոթերորդ',
+    8: 'ութերորդ',
+    9: 'իններորդ'
+}
+
+ORDINAL_TEENS = {
+    10: 'տասներորդ',
+    11: 'տասնմեկերորդ',
+    12: 'տասներկուերորդ',
+    13: 'տասներեքերորդ',
+    14: 'տասնչորսերորդ',
+    15: 'տասնհինգերորդ',
+    16: 'տասնվեցերորդ',
+    17: 'տասնյոթերորդ',
+    18: 'տասնութերորդ',
+    19: 'տասնիներորդ'
+}
+
+ORDINAL_TENS = {
+    2: 'քսաներորդ',
+    3: 'երեսուներորդ',
+    4: 'քառասուներորդ',
+    5: 'հիսուներորդ',
+    6: 'վաթսուներորդ',
+    7: 'յոթանասուներորդ',
+    8: 'ութսուներորդ',
+    9: 'իննսուներորդ'
+}
+
+class Num2Word_HY(Num2Word_Base):
+    CURRENCY_FORMS = {
+        'AMD': (('դրամ', 'դրամ'), ('լումա', 'լումա')),
+        'EUR': (('եվրո', 'եվրո'), ('ցենտ', 'ցենտ')),
+        'RUB': (('ռուբլի', 'ռուբլի'), ('կոպեկ', 'կոպեկ')),
+        'USD': (('դոլար', 'դոլար'), ('ցենտ', 'ցենտ')),
+        'JPY': (('իեն', 'իեն'), ('սեն', 'սեն')),
+        'GBP': (('ֆունտ ստեռլինգ', 'ֆունտ ստեռլինգ'), ('պենս', 'պենս')),
+        'CHF': (('շվեյցարական ֆրանկ', 'շվեյցարական ֆրանկ'), ('սանտիմ', 'սանտիմ')),
+        'CNY': (('յուան', 'յուան'), ('ֆեն', 'ֆեն')),
+        'IRR': (('իրանական ռիալ', 'իրանական ռիալ'), ('դինար', 'դինար')),
+        'TRY': (('թուրքական լիրա', 'թուրքական լիրա'), ('ղուրուշ', 'ղուրուշ')),
+        'AED': (('արաբական դիրհամ', 'արաբական դիրհամ'), ('ֆիլս', 'ֆիլս'))
+    }
+
+    def set_high_numwords(self, high):
+        max = 3 + 10 * len(high)
+        for word, n in zip(high, range(max, 3, -10)):
+            self.cards[10 ** n] = word
+
+    def setup(self):
+        self.negword = "մինուս "
+        self.pointword = "ամբողջ"
+        self.exclude_title = ["և", "ամբողջ", "մինուս"]
+        
+        self.high_numwords = [(10**12, "տրիլիոն"), (10**9, "միլիարդ"),
+                             (10**6, "միլիոն")]
+        self.mid_numwords = [(1000, "հազար"), (100, "հարյուր"),
+                             (90, "իննսուն"), (80, "ութսուն"), (70, "յոթանասուն"),
+                             (60, "վաթսուն"), (50, "հիսուն"), (40, "քառասուն"),
+                             (30, "երեսուն"), (20, "քսան")]
+        self.low_numwords = ["տասնինը", "տասնութ", "տասնյոթ", "տասնվեց", "տասնհինգ",
+                            "տասնչորս", "տասներեք", "տասներկու", "տասնմեկ", "տասը", 
+                            "ինը", "ութ", "յոթ", "վեց", "հինգ", "չորս", "երեք", 
+                            "երկու", "մեկ", "զրո"]
+    
+    def merge(self, curr, next):
+        ctext, cnum, ntext, nnum = curr + next
+
+        if cnum == 1:
+            # For 1000, don't need to add "մեկ"
+            if nnum == 1000:
+                return next
+            if nnum < 1000:
+                return next
+            ctext = "մեկ"
+        
+        if nnum < cnum and cnum >= 100 and cnum < 1000:
+            if nnum % 100 == 0:
+                ntext = ntext[:-1] + "ի"
+            return (ctext + " " + ntext, cnum + nnum)
+            
+        if nnum < 100:
+            if cnum < 100:
+                # Always add space between tens and ones
+                return ("%s %s" % (ctext, ntext), cnum + nnum)
+            if nnum < 10 and cnum in [100, 200, 300, 400, 500, 600, 700, 800, 900]:
+                return ("%s %s" % (ctext, ntext), cnum + nnum)
+            return ("%s %s" % (ctext, ntext), cnum + nnum)
+            
+        return ("%s %s" % (ctext, ntext), cnum + nnum)
+    
+    def to_cardinal(self, value):
+        if value == 0:
+            return 'զրո'
+        
+        # Simple cases
+        if value == 1000:
+            return 'հազար'
+            
+        # For millions and billions
+        if value == 1000000:
+            return 'մեկ միլիոն'
+        elif value % 1000000 == 0 and value < 1000000000:
+            prefix = value // 1000000
+            if prefix == 2:
+                return 'երկու միլիոն'
+            else:
+                return '%s միլիոն' % self.to_cardinal(prefix)
+                
+        if value == 1000000000:
+            return 'մեկ միլիարդ'
+        elif value % 1000000000 == 0 and value < 10**12:
+            prefix = value // 1000000000
+            if prefix == 2:
+                return 'երկու միլիարդ'
+            else:
+                return '%s միլիարդ' % self.to_cardinal(prefix)
+                
+        # For other cases use standard implementation
+        result = super(Num2Word_HY, self).to_cardinal(value)
+        
+        # Fix for numbers like X000000 and X000000000
+        if 'հազար հազար' in result:
+            result = result.replace('հազար հազար', 'միլիոն')
+        if 'հազար միլիոն' in result:
+            result = result.replace('հազար միլիոն', 'միլիարդ')
+        
+        return result
+    
+    def to_ordinal(self, value):
+        if value == 0:
+            return 'զրոերորդ'
+        
+        if value < 20:
+            if value < 10:
+                return ORDINAL_ONES[value]
+            else:
+                return ORDINAL_TEENS[value]
+                
+        if value < 100:
+            tens, units = divmod(value, 10)
+            if units == 0:
+                return ORDINAL_TENS[tens]
+            return TENS[tens] + " " + ORDINAL_ONES[units]
+            
+        # For larger numbers use simple rule - add "երորդ" at the end
+        cardinal = self.to_cardinal(value)
+        return cardinal + "երորդ"
+    
+    def to_ordinal_num(self, value):
+        self.verify_ordinal(value)
+        return str(value) + "-րդ"
+    
+    def pluralize(self, n, forms):
+        # Armenian plural rules:
+        # - If number ends with 1 (except 11), use singular form
+        # - For all other numbers use plural form
+        if forms:
+            if len(forms) >= 2:
+                if n == 1 or (n % 10 == 1 and n % 100 != 11):
+                    return forms[0]
+                return forms[1]
+            return forms[0]
+        return ''
+    
+    def to_year(self, val, longval=True):
+        if val < 0:
+            return self.to_cardinal(abs(val)) + " թվականից առաջ"
+        
+        # Special case for year: for 1000-1999, remove "մեկ" before "հազար"
+        if 1000 <= val < 2000:
+            year_str = self.to_cardinal(val)
+            if year_str.startswith("մեկ հազար"):
+                year_str = year_str[4:].strip()  # Remove "մեկ " at beginning
+            return year_str + " թվական"
+            
+        return self.to_cardinal(val) + " թվական"
+        
+    def to_currency(self, val, currency='EUR', cents=True):
+        # For currency with fractional values, e.g. 1.5 dollars
+        if cents and val != int(val):
+            # Get integer part
+            integer_part = int(val)
+            # Get decimal part
+            decimal_part = val - integer_part
+            
+            # Special cases for fractional values
+            if decimal_part == 0.5:
+                return '%s %s %s %s %s %s' % (
+                    self.to_cardinal(integer_part),
+                    self.pluralize(integer_part, self.CURRENCY_FORMS[currency][0]),
+                    self.pointword,
+                    self.to_cardinal(5),
+                    'տասներորդ',
+                    self.pluralize(5, self.CURRENCY_FORMS[currency][1])
+                )
+            elif decimal_part == 0.25:
+                return '%s %s %s %s %s %s' % (
+                    self.to_cardinal(integer_part),
+                    self.pluralize(integer_part, self.CURRENCY_FORMS[currency][0]),
+                    self.pointword,
+                    self.to_cardinal(25),
+                    'հարյուրերորդ',
+                    self.pluralize(25, self.CURRENCY_FORMS[currency][1])
+                )
+            elif decimal_part == 0.75:
+                return '%s %s %s %s %s %s' % (
+                    self.to_cardinal(integer_part),
+                    self.pluralize(integer_part, self.CURRENCY_FORMS[currency][0]),
+                    self.pointword,
+                    self.to_cardinal(75),
+                    'հարյուրերորդ',
+                    self.pluralize(75, self.CURRENCY_FORMS[currency][1])
+                )
+            
+        # For other cases use standard implementation
+        result = super(Num2Word_HY, self).to_currency(val, currency, cents)
+        
+        # If amount is whole (no cents), remove part with zero cents
+        if cents:
+            if int(val * 100) % 100 == 0:
+                parts = result.split(", ")
+                if len(parts) > 1:
+                    return parts[0]
+        
+        return result
+    
+    def to_numeral(self, value):
+        """
+        Returns the number as Armenian digits.
+        In modern Armenian, Arabic digits are used.
+        """
+        return str(value)
---
+++ tests/test_hy.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2003, Taro Ogawa.  All Rights Reserved.
+# Copyright (c) 2013, Savoir-faire Linux inc.  All Rights Reserved.
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301 USA
+
+from __future__ import unicode_literals
+
+from unittest import TestCase
+
+from num2words import num2words
+
+
+class Num2WordsHYTest(TestCase):
+    def test_cardinal(self):
+        self.assertEqual(num2words(0, lang='hy'), 'զրո')
+        self.assertEqual(num2words(1, lang='hy'), 'մեկ')
+        self.assertEqual(num2words(2, lang='hy'), 'երկու')
+        self.assertEqual(num2words(3, lang='hy'), 'երեք')
+        self.assertEqual(num2words(5, lang='hy'), 'հինգ')
+        self.assertEqual(num2words(9, lang='hy'), 'ինը')
+        self.assertEqual(num2words(10, lang='hy'), 'տասը')
+        self.assertEqual(num2words(11, lang='hy'), 'տասնմեկ')
+        self.assertEqual(num2words(12, lang='hy'), 'տասներկու')
+        self.assertEqual(num2words(15, lang='hy'), 'տասնհինգ')
+        self.assertEqual(num2words(19, lang='hy'), 'տասնինը')
+        self.assertEqual(num2words(20, lang='hy'), 'քսան')
+        self.assertEqual(num2words(21, lang='hy'), 'քսան մեկ')
+        self.assertEqual(num2words(30, lang='hy'), 'երեսուն')
+        self.assertEqual(num2words(50, lang='hy'), 'հիսուն')
+        self.assertEqual(num2words(99, lang='hy'), 'իննսուն ինը')
+        self.assertEqual(num2words(100, lang='hy'), 'հարյուր')
+        self.assertEqual(num2words(101, lang='hy'), 'հարյուր մեկ')
+        self.assertEqual(num2words(111, lang='hy'), 'հարյուր տասնմեկ')
+        self.assertEqual(num2words(120, lang='hy'), 'հարյուր քսան')
+        self.assertEqual(num2words(200, lang='hy'), 'երկու հարյուր')
+        self.assertEqual(num2words(999, lang='hy'), 'ինը հարյուր իննսուն ինը')
+        self.assertEqual(num2words(1000, lang='hy'), 'հազար')
+        self.assertEqual(num2words(1001, lang='hy'), 'հազար մեկ')
+        self.assertEqual(num2words(1111, lang='hy'), 'հազար հարյուր տասնմեկ')
+        self.assertEqual(num2words(2000, lang='hy'), 'երկու հազար')
+        self.assertEqual(num2words(10000, lang='hy'), 'տասը հազար')
+        self.assertEqual(num2words(100000, lang='hy'), 'հարյուր հազար')
+        self.assertEqual(num2words(1000000, lang='hy'), 'մեկ միլիոն')
+        self.assertEqual(num2words(2000000, lang='hy'), 'երկու միլիոն')
+        self.assertEqual(num2words(1000000000, lang='hy'), 'մեկ միլիարդ')
+
+    def test_ordinal(self):
+        self.assertEqual(num2words(1, lang='hy', to='ordinal'), 'առաջին')
+        self.assertEqual(num2words(2, lang='hy', to='ordinal'), 'երկրորդ')
+        self.assertEqual(num2words(3, lang='hy', to='ordinal'), 'երրորդ')
+        self.assertEqual(num2words(5, lang='hy', to='ordinal'), 'հինգերորդ')
+        self.assertEqual(num2words(9, lang='hy', to='ordinal'), 'իններորդ')
+        self.assertEqual(num2words(10, lang='hy', to='ordinal'), 'տասներորդ')
+        self.assertEqual(num2words(11, lang='hy', to='ordinal'), 'տասնմեկերորդ')
+        self.assertEqual(num2words(12, lang='hy', to='ordinal'), 'տասներկուերորդ')
+        self.assertEqual(num2words(20, lang='hy', to='ordinal'), 'քսաներորդ')
+        self.assertEqual(num2words(21, lang='hy', to='ordinal'), 'քսան առաջին')
+        self.assertEqual(num2words(101, lang='hy', to='ordinal'), 'հարյուր մեկերորդ')
+
+    def test_ordinal_num(self):
+        self.assertEqual(num2words(1, lang='hy', to='ordinal_num'), '1-րդ')
+        self.assertEqual(num2words(2, lang='hy', to='ordinal_num'), '2-րդ')
+        self.assertEqual(num2words(10, lang='hy', to='ordinal_num'), '10-րդ')
+
+    def test_currency(self):
+        self.assertEqual(
+            num2words(1.0, lang='hy', to='currency', currency='AMD'),
+            'մեկ դրամ'
+        )
+        self.assertEqual(
+            num2words(2.0, lang='hy', to='currency', currency='AMD'),
+            'երկու դրամ'
+        )
+        self.assertEqual(
+            num2words(1.5, lang='hy', to='currency', currency='USD'),
+            'մեկ դոլար ամբողջ հինգ տասներորդ ցենտ'
+        )
+        self.assertEqual(
+            num2words(100.0, lang='hy', to='currency', currency='EUR'),
+            'հարյուր եվրո'
+        )
+
+    def test_year(self):
+        self.assertEqual(num2words(2020, lang='hy', to='year'), 'երկու հազար քսան թվական')
+        self.assertEqual(num2words(1990, lang='hy', to='year'), 'հազար ինը հարյուր իննսուն թվական')
+        self.assertEqual(num2words(-44, lang='hy', to='year'), 'քառասուն չորս թվականից առաջ')
